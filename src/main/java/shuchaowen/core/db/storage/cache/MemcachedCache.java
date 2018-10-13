0929e14fa1658fb1ba6a279d1a0557756b2e9afb
wcnnkh <247044007@qq.com>
2018-10-13 20:55:45 +0800

    优化代码

--- src/main/java/shuchaowen/core/db/storage/cache/MemcachedCache.java
+++ src/main/java/shuchaowen/core/db/storage/cache/MemcachedCache.java
@@ -8,19 +8,9 @@ public class MemcachedCache implements Cache{
 	public MemcachedCache(Memcached memcached){
 		this.memcached = memcached;
 	}
-	
-	
 
 	public <T> T getAndTouch(Class<T> type, String key, int exp) {
-		T t = memcached.get(key);;
-		if (t == null) {
-			return t;
-		}
-
-		if (exp > 0) {
-			memcached.set(key, exp, t);
-		}
-		return t;
+		return memcached.getAndTocuh(key, exp);
 	}
 
 	public void set(String key, int exp, Object data) {
--- src/main/java/shuchaowen/core/db/storage/memcached/MemcachedHotSpotCacheStorage.java
+++ src/main/java/shuchaowen/core/db/storage/memcached/MemcachedHotSpotCacheStorage.java
@@ -4,6 +4,7 @@ import shuchaowen.core.cache.Memcached;
 import shuchaowen.core.db.AbstractDB;
 import shuchaowen.core.db.storage.AbstractCacheStorage;
 import shuchaowen.core.db.storage.Storage;
+import shuchaowen.core.db.storage.cache.MemcachedBinaryCache;
 import shuchaowen.core.db.storage.cache.MemcachedCache;
 import shuchaowen.core.util.XTime;
 
@@ -11,10 +12,14 @@ public class MemcachedHotSpotCacheStorage extends AbstractCacheStorage{
 	private static final int DEFAULT_EXP = (int) ((7 * XTime.ONE_DAY) / 1000);
 	
 	public MemcachedHotSpotCacheStorage(AbstractDB db, Memcached memcached, Storage storage){
-		super(db, new MemcachedCache(memcached), "", DEFAULT_EXP, storage);
+		super(db, new MemcachedBinaryCache(memcached), "", DEFAULT_EXP, storage);
 	}
 	
 	public MemcachedHotSpotCacheStorage(AbstractDB db, String prefix, int exp, Memcached memcached, Storage storage){
-		super(db, new MemcachedCache(memcached), prefix, exp, storage);
+		super(db, new MemcachedBinaryCache(memcached), prefix, exp, storage);
+	}
+	
+	public MemcachedHotSpotCacheStorage(AbstractDB db, String prefix, int exp, MemcachedCache memcachedCache, Storage storage){
+		super(db, memcachedCache, prefix, exp, storage);
 	}
 }
