43281f24eaba5cb5d63713692acc47c37d4b5984
wcnnkh <247044007@qq.com>
2018-10-24 21:49:32 +0800

    整理代码

---
+++ src/main/java/shuchaowen/core/db/DataSource.java
@@ -0,0 +1,23 @@
+package shuchaowen.core.db;
+
+import java.io.Closeable;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource extends DB{
+	private final javax.sql.DataSource dataSource;
+	
+	public DataSource(javax.sql.DataSource dataSource){
+		this.dataSource = dataSource;
+	}
+	
+	public Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+
+	public void close() throws Exception {
+		if(dataSource instanceof Closeable){
+			((Closeable) dataSource).close();
+		}
+	}
+}
