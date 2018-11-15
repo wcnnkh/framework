2bebbb366e029bc4226cc864ee7dea12365bb4c3
wcnnkh <247044007@qq.com>
2018-11-15 11:58:24 +0800

    优化MQ

---
+++ src/main/java/shuchaowen/mq/ConsumerRunnable.java
@@ -0,0 +1,20 @@
+package shuchaowen.mq;
+
+import shuchaowen.core.exception.ShuChaoWenRuntimeException;
+
+public final class ConsumerRunnable<T> implements Runnable{
+	private final Consumer<T> consumer;
+	private final T message;
+	
+	public ConsumerRunnable(Consumer<T> consumer, T message){
+		this.consumer = consumer;
+		this.message = message;
+	}
+	public void run() {
+		try {
+			consumer.handler(message);
+		} catch (Exception e) {
+			throw new ShuChaoWenRuntimeException(e);
+		}
+	}
+}
