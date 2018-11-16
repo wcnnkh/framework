f56ede0ee07ff98159a5826291deac6ac748b49e
wcnnkh <247044007@qq.com>
2018-11-16 17:24:52 +0800

    优化微信支付封装

--- src/main/java/shuchaowen/tencent/weixin/WeiXinUtils.java
+++ src/main/java/shuchaowen/tencent/weixin/WeiXinUtils.java
@@ -1,7 +1,5 @@
 package shuchaowen.tencent.weixin;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +12,6 @@ import org.dom4j.Node;
 import com.alibaba.fastjson.JSONObject;
 
 import shuchaowen.core.util.SignHelp;
-import shuchaowen.web.util.http.HttpPost;
 import shuchaowen.web.util.http.core.Http;
 
 /**
@@ -23,7 +20,6 @@ import shuchaowen.web.util.http.core.Http;
 public final class WeiXinUtils {
 	public static final String weixin_authorize_url = "https://open.weixin.qq.com/connect/oauth2/authorize";
 	public static final String weixin_qrconnect_url = "https://open.weixin.qq.com/connect/qrconnect";
-	public static final String weixin_unifiedorder_url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
 
 	private WeiXinUtils(){};
 	
@@ -78,97 +74,6 @@ public final class WeiXinUtils {
 		default:
 			return 7 * 24 * 3600;
 		}
-	}
-
-	/**
-	 *  统一下单 字段说明见：https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_1
-	 * @param appid 微信支付分配的公众账号ID（企业号corpid即为此appId）
-	 * @param mch_id 微信支付分配的商户号
-	 * @param device_info 自定义参数，可以为终端设备号(门店号或收银设备ID)，PC网页或公众号内支付可以传"WEB"
-	 * @param nonce_str 随机字符串，长度要求在32位以内。推荐随机数生成算法
-	 * @param sign  通过签名算法计算得出的签名值，详见签名生成算法
-	 * @param sign_type 签名类型，默认为MD5，支持HMAC-SHA256和MD5。
-	 * @param body 商品简单描述，该字段请按照规范传递，具体请见参数规定
-	 * @param detail
-	 * @param attach
-	 * @param out_trade_no
-	 * @param fee_type
-	 * @param total_fee
-	 * @param spbill_create_ip
-	 * @param time_start
-	 * @param time_expire
-	 * @param goods_tag
-	 * @param notify_url
-	 * @param trade_type
-	 * @param product_id
-	 * @param limit_pay
-	 * @param openid
-	 * @return
-	 */
-	public static String getUnifiedorder(String appId, String mch_id, String apiKey,
-			String device_info, String nonce_str,
-			String body, String detail, String attach,
-			String out_trade_no, String fee_type, int total_fee, String spbill_create_ip,
-			String time_start, String time_expire, String goods_tag, String notify_url,
-			String trade_type, String product_id, String limit_pay, String openid){
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("appid", appId);
-		map.put("mch_id", mch_id);
-		map.put("device_info", device_info);
-		map.put("nonce_str", nonce_str);
-		map.put("sign_type", "MD5");
-		map.put("body", body);
-		map.put("detail", detail);
-		map.put("attach", attach);
-		map.put("out_trade_no", out_trade_no);
-		map.put("fee_type", fee_type);
-		map.put("total_fee", total_fee +"");
-		map.put("spbill_create_ip",spbill_create_ip);
-		map.put("time_start", time_start);
-		map.put("time_expire", time_expire);
-		map.put("goods_tag", goods_tag);
-		map.put("notify_url", notify_url);
-		map.put("trade_type", trade_type);
-		map.put("product_id", product_id);
-		map.put("limit_pay", limit_pay);
-		map.put("openid", openid);
-		String[] keys = map.keySet().toArray(new String[0]);
-		Arrays.sort(keys);
-		String v;
-		String k;
-		StringBuilder sb = new StringBuilder();
-		Element root = DocumentHelper.createElement("xml");
-		Element element;
-		for(int i=0; i<keys.length; i++){ 
-			k = keys[i];
-			if(k == null){
-				continue;
-			}
-			
-			v = map.get(k);
-			if(v == null){
-				continue;
-			}
-			
-			element = DocumentHelper.createElement(k);
-			element.setText(v);
-			root.add(element);
-			if(sb.length() > 0){
-				sb.append("&");
-			}
-			
-			sb.append(k).append("=").append(v);
-		}
-		sb.append("&key=").append(apiKey);
-		element = DocumentHelper.createElement("sign");
-		element.setText(SignHelp.md5UpperStr(sb.toString(), "UTF-8"));
-		root.add(element);
-		try {
-			return HttpPost.invoke(weixin_unifiedorder_url, root.asXML().getBytes("UTF-8"), null, 5000, 5000);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 	
 	public static String getPaySign(Map<String, String> paramMap, String apiKey){
