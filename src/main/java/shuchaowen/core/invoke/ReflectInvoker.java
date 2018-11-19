288b13e7e711bb8a0b1f4b62d7a8eec685e258c0
wcnnkh <247044007@qq.com>
2018-11-19 14:48:47 +0800

    优化错误提示

--- src/main/java/shuchaowen/core/invoke/ReflectInvoker.java
+++ src/main/java/shuchaowen/core/invoke/ReflectInvoker.java
@@ -6,8 +6,8 @@ import java.lang.reflect.Modifier;
 import shuchaowen.core.beans.BeanFactory;
 
 public class ReflectInvoker implements Invoker {
-	private Object bean;// 如果是静态方法这个就应该是空
-	private Method method;
+	private final Object bean;// 如果是静态方法这个就应该是空
+	private final Method method;
 	
 	public ReflectInvoker(BeanFactory beanFactory, Class<?> type, Method method) {
 		this.bean = Modifier.isStatic(method.getModifiers())? null:beanFactory.get(type);
@@ -23,5 +23,10 @@ public class ReflectInvoker implements Invoker {
 
 	public Object invoke(Object... args) throws Throwable {
 		return method.invoke(bean, args);
+	}
+	
+	@Override
+	public String toString() {
+		return method.toString();
 	}
 }
