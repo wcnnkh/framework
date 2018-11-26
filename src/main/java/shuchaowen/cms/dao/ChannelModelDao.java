224561cb432d4efd5b36c767bad30c940f10f011
wcnnkh <247044007@qq.com>
2018-11-26 11:57:48 +0800

    删除CMS

--- src/main/java/shuchaowen/cms/dao/ChannelModelDao.java
+++
@@ -1,9 +0,0 @@
-package shuchaowen.cms.dao;
-
-import shuchaowen.cms.pojo.ChannelModel;
-
-public interface ChannelModelDao {
-	ChannelModel get(long id);
-	
-	ChannelModel create(String name, int weight);
-}
