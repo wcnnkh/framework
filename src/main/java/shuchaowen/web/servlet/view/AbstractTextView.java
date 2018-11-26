2751895552331743c515c6f6cdff6a47191a4101
wcnnkh <247044007@qq.com>
2018-11-26 14:13:43 +0800

    整理代码

--- src/main/java/shuchaowen/web/servlet/view/AbstractTextView.java
+++ src/main/java/shuchaowen/web/servlet/view/AbstractTextView.java
@@ -5,19 +5,20 @@ import java.io.IOException;
 import shuchaowen.core.http.enums.ContentType;
 import shuchaowen.core.util.Logger;
 import shuchaowen.web.servlet.View;
+import shuchaowen.web.servlet.WebRequest;
 import shuchaowen.web.servlet.WebResponse;
 
 public abstract class AbstractTextView implements View{
 	public abstract String getResponseText();
 	
-	public void render(WebResponse response) throws IOException{
+	public void render(WebRequest request, WebResponse response) throws IOException{
 		if(response.getContentType() == null){
 			response.setContentType(ContentType.TEXT_HTML.getValue());
 		}
 		
 		String responseText = getResponseText();
 		if(response.getRequest().isDebug()){
-			Logger.debug("RESPONSE["+(System.currentTimeMillis() - response.getRequest().getCreateTime())+"ms]", responseText);
+			Logger.debug("RESPONSE["+(System.currentTimeMillis() - request.getCreateTime())+"ms]", responseText);
 		}
 		
 		response.write(responseText);
