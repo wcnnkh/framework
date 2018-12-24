68ad4a6752d8c4c4262909891411bac67d191cc1
wcnnkh <247044007@qq.com>
2018-12-24 19:43:16 +0800

    整理代码

--- src/main/java/scw/timer/AbstractScheduledExecutorService.java
+++ src/main/java/scw/timer/AbstractScheduledExecutorService.java
@@ -4,43 +4,52 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import scw.common.RunnableStaticProxy;
+import scw.common.utils.XUtils;
+import scw.locks.Lock;
+import scw.locks.LockFactory;
 
-public class AbstractScheduledExecutorService implements ScheduledExecutorService{
-	private java.util.concurrent.ScheduledExecutorService service;
-	
-	public AbstractScheduledExecutorService(int corePoolSize){
+public class AbstractScheduledExecutorService implements ScheduledExecutorService {
+	private final java.util.concurrent.ScheduledExecutorService service;
+	private final LockFactory lockFactory;
+
+	public AbstractScheduledExecutorService(int corePoolSize, LockFactory lockFactory) {
 		this.service = Executors.newScheduledThreadPool(corePoolSize);
+		this.lockFactory = lockFactory;
 	}
-	
+
 	public void scheduleAtFixedRate(String key, Runnable command, long initialDelay, long period, TimeUnit unit) {
-		service.scheduleAtFixedRate(new RunnableStaticProxy(command) {
-			
-			@Override
-			protected boolean before() {
-				
-				return false;
-			}
-			
-			@Override
-			protected void after() {
-				//ignore
-			}
-		}, initialDelay, period, unit);
+		service.scheduleAtFixedRate(new Task(command, lockFactory), initialDelay, period, unit);
 	}
 
 	public void schedule(Runnable command, long delay, TimeUnit unit) {
-		// TODO Auto-generated method stub
-		
+		service.schedule(new Task(command, lockFactory), delay, unit);
+
 	}
 
 	public void scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
-		// TODO Auto-generated method stub
-		
+		service.scheduleWithFixedDelay(new Task(command, lockFactory), initialDelay, delay, unit);
 	}
 
 	public void scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
-		// TODO Auto-generated method stub
-		
+		service.scheduleAtFixedRate(new Task(command, lockFactory), initialDelay, period, unit);
+	}
+}
+
+class Task extends RunnableStaticProxy {
+	private Lock lock;
+
+	public Task(Runnable command, LockFactory lockFactory) {
+		super(command);
+		this.lock = lockFactory.getLock(this.getClass().getName() + "#" + XUtils.getUUID());
 	}
 
+	@Override
+	protected boolean before() {
+		return lock.lock();
+	}
+
+	@Override
+	protected void after() {
+		lock.unlock();
+	}
 }
