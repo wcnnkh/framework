4254ccbb1f7bad9afd43cbf0d5d150bc121a2d4c
wcnnkh <247044007@qq.com>
2018-12-29 14:02:23 +0800

    修改BUG

--- src/main/java/scw/beans/FieldListenMethodInterceptor.java
+++ src/main/java/scw/beans/FieldListenMethodInterceptor.java
@@ -17,9 +17,13 @@ public final class FieldListenMethodInterceptor implements MethodInterceptor, Be
 	private transient ClassInfo classInfo;
 
 	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+		if (classInfo == null) {
+			classInfo = ClassUtils.getClassInfo(obj.getClass());
+		}
+
 		if (args.length == 0) {
 			if (BeanFieldListen.START_LISTEN.equals(method.getName())) {
-				if (BeanFieldListen.class.isAssignableFrom(method.getDeclaringClass())) {
+				if (BeanFieldListen.class.isAssignableFrom(classInfo.getClz())) {
 					startListen = true;
 					return proxy.invokeSuper(obj, args);
 				} else {
@@ -27,7 +31,7 @@ public final class FieldListenMethodInterceptor implements MethodInterceptor, Be
 					return null;
 				}
 			} else if (BeanFieldListen.GET_CHANGE_MAP.equals(method.getName())) {
-				if (BeanFieldListen.class.isAssignableFrom(method.getDeclaringClass())) {
+				if (BeanFieldListen.class.isAssignableFrom(classInfo.getClz())) {
 					return proxy.invokeSuper(obj, args);
 				} else {
 					return get_field_change_map();
@@ -36,17 +40,13 @@ public final class FieldListenMethodInterceptor implements MethodInterceptor, Be
 		}
 
 		if (startListen) {
-			if (classInfo == null) {
-				classInfo = ClassUtils.getClassInfo(obj.getClass());
-			}
-
 			FieldInfo fieldInfo = classInfo.getFieldInfoBySetterName(method.getName());
 			if (fieldInfo != null) {
 				Object rtn;
 				Object oldValue = null;
 				oldValue = fieldInfo.forceGet(obj);
 				rtn = proxy.invokeSuper(obj, args);
-				if (BeanFieldListen.class.isAssignableFrom(method.getDeclaringClass())) {
+				if (BeanFieldListen.class.isAssignableFrom(classInfo.getClz())) {
 					((BeanFieldListen) obj).field_change(fieldInfo, oldValue);
 				} else {
 					field_change(fieldInfo, oldValue);
