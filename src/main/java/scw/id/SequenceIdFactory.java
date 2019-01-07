e51d5c161af520a086a7b33ca3bae9fc91016a4e
wcnnkh <247044007@qq.com>
2019-01-07 11:23:42 +0800

    整理代码

--- src/main/java/scw/id/SequenceIdFactory.java
+++ src/main/java/scw/id/SequenceIdFactory.java
@@ -1,16 +1,26 @@
 package scw.id;
 
+import scw.common.utils.StringUtils;
 import scw.common.utils.XTime;
 import scw.memcached.Memcached;
 import scw.redis.Redis;
 
 public class SequenceIdFactory implements IdFactory<SequenceId> {
+	private static final String DEFAULT_TIME_FORMAT = "yyyyMMddHHmmss";
 	private final IdFactory<Long> idFactory;
 	private final String time_format;
+
+	public SequenceIdFactory(Memcached memcached) {
+		this(memcached, DEFAULT_TIME_FORMAT);
+	}
 
 	public SequenceIdFactory(Memcached memcached, String time_format) {
 		this.idFactory = new MemcachedIdFactory(memcached);
 		this.time_format = time_format;
+	}
+
+	public SequenceIdFactory(Redis redis) {
+		this(redis, DEFAULT_TIME_FORMAT);
 	}
 
 	public SequenceIdFactory(Redis redis, String time_format) {
@@ -25,6 +35,13 @@ public class SequenceIdFactory implements IdFactory<SequenceId> {
 			number = Long.MAX_VALUE + number;
 		}
 
-		return new SequenceId(t, XTime.format(t, time_format) + Long.toString(number, Character.MAX_RADIX));
+		String id;
+		if (StringUtils.isNull(time_format)) {
+			id = Long.toString(t, Character.MAX_RADIX);
+			id = StringUtils.reversed(id) + Long.toString(number, Character.MAX_RADIX);
+		} else {
+			id = XTime.format(t, time_format) + Long.toString(number, Character.MAX_RADIX);
+		}
+		return new SequenceId(t, id);
 	}
 }
--- src/main/java/scw/id/SequenceIdGenerator.java
+++ src/main/java/scw/id/SequenceIdGenerator.java
@@ -1,29 +1,29 @@
 package scw.id;
 
-import scw.common.utils.XTime;
 import scw.memcached.Memcached;
 import scw.redis.Redis;
 
 public class SequenceIdGenerator implements IdGenerator<SequenceId> {
-	private final IdFactory<Long> idFactory;
-	private final String time_format;
+	private static final String DEFAULT_TIME_FORMAT = "yyyyMMddHHmmss";
+	private final IdFactory<SequenceId> idFactory;
+
+	public SequenceIdGenerator(Memcached memcached) {
+		this(memcached, DEFAULT_TIME_FORMAT);
+	}
 
 	public SequenceIdGenerator(Memcached memcached, String time_format) {
-		this.idFactory = new MemcachedIdFactory(memcached);
-		this.time_format = time_format;
+		this.idFactory = new SequenceIdFactory(memcached, time_format);
+	}
+
+	public SequenceIdGenerator(Redis redis) {
+		this(redis, DEFAULT_TIME_FORMAT);
 	}
 
 	public SequenceIdGenerator(Redis redis, String time_format) {
-		this.idFactory = new RedisIdFactory(redis);
-		this.time_format = time_format;
+		this.idFactory = new SequenceIdFactory(redis, time_format);
 	}
 
 	public SequenceId next() {
-		long t = System.currentTimeMillis();
-		long number = idFactory.generator(this.getClass().getName());
-		if (number < 0) {
-			number = Long.MAX_VALUE + number;
-		}
-		return new SequenceId(t, XTime.format(t, time_format) + Long.toString(number, Character.MAX_RADIX));
+		return idFactory.generator(this.getClass().getName());
 	}
 }
