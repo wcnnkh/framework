2eff348f5a3299e93df39a6509c7ea6169327b89
wcnnkh <247044007@qq.com>
2019-01-18 12:48:05 +0800

    整理代码

--- src/main/java/scw/db/DruidDB.java
+++ src/main/java/scw/db/DruidDB.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import com.alibaba.druid.pool.DruidDataSource;
 
 import scw.common.utils.ConfigUtils;
+import scw.common.utils.PropertiesUtils;
 import scw.common.utils.StringUtils;
 import scw.db.sql.SQLFormat;
 
@@ -19,13 +20,13 @@ public final class DruidDB extends DB {
 
 	public DruidDB(SQLFormat sqlFormat, Properties properties) {
 		super(sqlFormat);
-		String url = getProperties(properties, "jdbcUrl", "url", "host");
-		String username = getProperties(properties, "username", "user", "name");
-		String password = getProperties(properties, "password", "pwd");
-		String minSize = getProperties(properties, "minSize", "initialSize", "min");
-		String maxSize = getProperties(properties, "maxSize", "maxActive", "max");
-		String driver = getProperties(properties, "driver", "driverClass", "driverClassName");
-		String maxPoolPreparedStatementPerConnectionSize = getProperties(properties,
+		String url = PropertiesUtils.getProperty(properties, "jdbcUrl", "url", "host");
+		String username = PropertiesUtils.getProperty(properties, "username", "user", "name");
+		String password = PropertiesUtils.getProperty(properties, "password", "pwd");
+		String minSize = PropertiesUtils.getProperty(properties, "minSize", "initialSize", "min");
+		String maxSize = PropertiesUtils.getProperty(properties, "maxSize", "maxActive", "max");
+		String driver = PropertiesUtils.getProperty(properties, "driver", "driverClass", "driverClassName");
+		String maxPoolPreparedStatementPerConnectionSize = PropertiesUtils.getProperty(properties,
 				"maxPoolPreparedStatementPerConnectionSize");
 
 		datasource = new DruidDataSource();
@@ -39,15 +40,6 @@ public final class DruidDB extends DB {
 		datasource.setMaxPoolPreparedStatementPerConnectionSize(
 				StringUtils.isEmpty(maxPoolPreparedStatementPerConnectionSize) ? 20
 						: Integer.parseInt(maxPoolPreparedStatementPerConnectionSize));
-	}
-
-	private String getProperties(Properties properties, String... key) {
-		for (String k : key) {
-			if (properties.contains(k)) {
-				return properties.get(k).toString();
-			}
-		}
-		return null;
 	}
 
 	public DruidDB(String url, String username, String password, int minSize, int maxSize) {
