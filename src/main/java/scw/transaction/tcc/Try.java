a859741b63ea10edbc8774078fa4f39828ae7fed
wcnnkh <247044007@qq.com>
2019-03-07 15:43:01 +0800

    整理代码

--- src/main/java/scw/transaction/tcc/Try.java
+++ src/main/java/scw/transaction/tcc/Try.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import scw.transaction.tcc.service.RetryTCCService;
+
 @Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Try {
@@ -18,5 +20,5 @@ public @interface Try {
 	 * TCC事务的服务方式， 默认是以定时器的方式重试
 	 * @return
 	 */
-	public Class<? extends TCCService> service() default TCCService.class;
+	public Class<? extends TCCService> service() default RetryTCCService.class;
 }
