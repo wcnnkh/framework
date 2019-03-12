a9c6b9b4ed0851354cfafe47862084be187390f7
wcnnkh <247044007@qq.com>
2019-03-12 17:38:23 +0800

    fix

--- src/main/java/scw/utils/apple/ApplePay.java
+++ src/main/java/scw/utils/apple/ApplePay.java
@@ -3,8 +3,10 @@ package scw.utils.apple;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import scw.common.Logger;
 import scw.common.utils.StringUtils;
 import scw.net.http.HttpUtils;
 
@@ -12,21 +14,36 @@ public final class ApplePay {
 	private static final String SANDBOX_URL = "https://sandbox.itunes.apple.com/verifyReceipt";
 	private static final String DEV_RUL = "https://buy.itunes.apple.com/verifyReceipt";
 	private final boolean shandbox;
+	private final boolean debug;
+
+	public ApplePay(boolean shandbox) {
+		this(shandbox, true);
+	}
 
 	/**
 	 * 是否沙盒模式
 	 * 
 	 * @param shandbox
 	 */
-	public ApplePay(boolean shandbox) {
+	public ApplePay(boolean shandbox, boolean debug) {
 		this.shandbox = shandbox;
+		this.debug = debug;
 	}
 
 	private String getPayUrl() {
 		return shandbox ? SANDBOX_URL : DEV_RUL;
 	}
 
-	public String pay(String receiptData, String password, String excludeOldTransactions) {
+	/**
+	 * @param receiptData
+	 *            前端给的支付凭据 base64
+	 * @param password
+	 *            可选
+	 * @param excludeOldTransactions
+	 *            可选
+	 * @return
+	 */
+	public JSONObject pay(String receiptData, String password, String excludeOldTransactions) {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("receipt-data", receiptData);
 		if (!StringUtils.isEmpty(password)) {
@@ -36,6 +53,35 @@ public final class ApplePay {
 		if (!StringUtils.isEmpty(excludeOldTransactions)) {
 			map.put("exclude-old-transactions", excludeOldTransactions);
 		}
-		return HttpUtils.doPost(getPayUrl(), null, JSONObject.toJSONString(map));
+
+		String response = HttpUtils.doPost(getPayUrl(), null, JSONObject.toJSONString(map));
+		if (debug) {
+			Logger.debug(this.getClass().getName(), response);
+		}
+
+		return JSONObject.parseObject(response);
+	}
+
+	public String getProductIdAndCheckReceipt(String receiptData, String password, String excludeOldTransactions) {
+		JSONObject json = pay(receiptData, password, excludeOldTransactions);
+		if (json.containsKey("status") && json.getIntValue("status") == 0) {
+			json = json.getJSONObject("receipt");
+			if (json.containsKey("in_app")) {// ios7
+				JSONArray in_app = json.getJSONArray("in_app");
+				if (in_app == null || in_app.size() == 0) {
+					return null;
+				}
+
+				json = in_app.getJSONObject(0);
+				if (json == null) {
+					return null;
+				}
+
+				return json.getString("product_id");
+			} else {
+				return json.getString("product_id");
+			}
+		}
+		return null;
 	}
 }
