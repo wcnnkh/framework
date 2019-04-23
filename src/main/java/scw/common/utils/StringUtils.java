6519497c03c9343433ccb54b67d65de4cdd0e37a
wcnnkh <247044007@qq.com>
2019-04-23 13:35:04 +0800

    优化字符串比较

--- src/main/java/scw/common/utils/StringUtils.java
+++ src/main/java/scw/common/utils/StringUtils.java
@@ -2008,11 +2008,10 @@ public final class StringUtils {
 	}
 
 	/**
-	 * 判断字符串是否与通配符匹配
+	 * 判断字符串是否与通配符匹配 只能存在通配符\\*和? ?代表1个 *代表多个
 	 * 
 	 * @param text
 	 * @param match
-	 *            只能存在通配符*
 	 * @return
 	 */
 	public static boolean test(String text, String match) {
@@ -2025,24 +2024,90 @@ public final class StringUtils {
 		}
 
 		if (match.indexOf("*") == -1) {
-			return text.equals(match);
+			if (match.indexOf("?") == -1) {
+				return text.equals(match);
+			} else {
+				return test(text, match, '?', false);
+			}
 		}
 
 		String[] arr = match.split("\\*");
-		int index = 0;
+		int begin = 0;
 		for (int i = 0; i < arr.length; i++) {
-			String m = arr[i];
-			if (m.length() == 0) {
-				continue;
-			}
-
-			int tempIndex = text.indexOf(m, index);
-			if (tempIndex == -1) {
+			String v = arr[i];
+			if (text.length() < v.length()) {
 				return false;
 			}
 
-			index = tempIndex + m.length();
+			boolean b = false;
+			int a = begin;
+			for (; a < text.length(); a++) {
+				int end = a + v.length();
+				if (end > text.length()) {
+					return false;
+				}
+
+				String c = text.substring(a, end);
+				if (test(c, v, '?', false)) {
+					b = true;
+					break;
+				}
+			}
+
+			if (!b) {
+				return false;
+			}
+
+			begin = a + v.length();
 		}
 		return true;
+	}
+
+	public static boolean test(String text, String match, char matchChar, boolean multiple) {
+		if (match.indexOf(matchChar) == -1) {
+			return text.equals(match);
+		}
+
+		int size = match.length();
+		if (multiple) {
+			int index = 0;
+			int findIndex = 0;
+			for (int i = 0; i < size; i++) {
+				char c = match.charAt(i);
+				if (c != matchChar) {
+					continue;
+				}
+
+				String v = match.substring(index, i);
+				index = i;
+				if (v.length() == 0) {
+					continue;
+				}
+
+				int tempIndex = text.indexOf(v, findIndex);
+				if (tempIndex == -1) {
+					return false;
+				}
+
+				findIndex = tempIndex + v.length();
+			}
+			return true;
+		} else {
+			if (text.length() != size) {
+				return false;
+			}
+
+			for (int i = 0; i < size; i++) {
+				if (match.charAt(i) == matchChar) {
+					continue;
+				}
+
+				if (match.charAt(i) != text.charAt(i)) {
+					return false;
+				}
+			}
+
+			return true;
+		}
 	}
 }
