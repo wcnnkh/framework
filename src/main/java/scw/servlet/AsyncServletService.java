1e2e88a22d04e1ea8e69ae23c5f311e4cf3be6b4
wcnnkh <247044007@qq.com>
2019-04-28 11:45:36 +0800

    fix

--- src/main/java/scw/servlet/AsyncServletService.java
+++ src/main/java/scw/servlet/AsyncServletService.java
@@ -1,5 +1,6 @@
 package scw.servlet;
 
+
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -18,60 +19,67 @@ import scw.core.utils.StringParseUtils;
 public class AsyncServletService extends DefaultServletService {
 	private static final String THREAD_CORE_SIZE = "servlet.thread.core.size";
 	private static final String THREAD_MAX_SIZE = "servlet.thread.max.size";
+	private static final String CONTAINER_THREAD_MANAGER = "servlet.thread.container";
 	private ThreadPoolExecutor executor;
 	private final int coreSize;
 	private final int maxSize;
+	/**
+	 * 把线程交给servlet容器来管理
+	 */
+	private final boolean containerThreadManager;
 
-	public AsyncServletService(BeanFactory beanFactory,
-			PropertiesFactory propertiesFactory, String configPath,
+	public AsyncServletService(BeanFactory beanFactory, PropertiesFactory propertiesFactory, String configPath,
 			String[] rootBeanFilters) throws Throwable {
 		super(beanFactory, propertiesFactory, configPath, rootBeanFilters);
-		this.coreSize = StringParseUtils.parseInt(
-				propertiesFactory.getValue(THREAD_CORE_SIZE), 16);
-		this.maxSize = StringParseUtils.parseInt(
-				propertiesFactory.getValue(THREAD_MAX_SIZE), 256);
+		this.coreSize = StringParseUtils.parseInt(propertiesFactory.getValue(THREAD_CORE_SIZE), 16);
+		this.maxSize = StringParseUtils.parseInt(propertiesFactory.getValue(THREAD_MAX_SIZE), 256);
+		this.containerThreadManager = StringParseUtils
+				.parseBoolean(propertiesFactory.getValue(CONTAINER_THREAD_MANAGER), false);
 	}
 
 	@Override
 	public void service(ServletRequest req, ServletResponse resp) {
 		if (req.isAsyncSupported() && !req.isAsyncStarted()) {
-			if (executor == null) {
-				synchronized (this) {
-					if (executor == null) {
-						executor = new ThreadPoolExecutor(coreSize, maxSize, 1,
-								TimeUnit.MINUTES,
-								new LinkedBlockingQueue<Runnable>());
+			Execute command = new Execute(req, resp);
+			if (containerThreadManager) {
+				command.defaultExecute();
+			} else {
+				if (executor == null) {
+					synchronized (this) {
+						if (executor == null) {
+							executor = new ThreadPoolExecutor(coreSize, maxSize, 1, TimeUnit.MINUTES,
+									new LinkedBlockingQueue<Runnable>());
+						}
 					}
 				}
+				executor.execute(command);
 			}
-
-			executor.execute(new Execute(req, resp));
 			return;
 		}
 		super.service(req, resp);
 	}
 
-	private void asyncService(ServletRequest req, ServletResponse resp) {
-		super.service(req, resp);
+	protected void asyncService(AsyncContext asyncContext) {
+		try {
+			super.service(asyncContext.getRequest(), asyncContext.getResponse());
+		} finally {
+			asyncContext.complete();
+		}
 	}
 
 	final class Execute implements Runnable {
-		private final ServletRequest req;
-		private final ServletResponse resp;
 		private final AsyncContext asyncContext;
 
 		public Execute(ServletRequest req, ServletResponse resp) {
-			this.req = req;
-			this.resp = resp;
 			this.asyncContext = req.startAsync(req, resp);
 		}
 
 		public void run() {
-			try {
-				asyncService(req, resp);
-			} finally {
-				asyncContext.complete();
-			}
+			asyncService(asyncContext);
+		}
+
+		public void defaultExecute() {
+			asyncContext.start(this);
 		}
 	}
 
@@ -81,4 +89,5 @@ public class AsyncServletService extends DefaultServletService {
 			executor.shutdownNow();
 		}
 	}
+
 }
