de4e9e44f3a1f22c86d598faa52e3116e81726f6
wcnnkh <247044007@qq.com>
2019-05-06 18:23:21 +0800

    fix

--- src/main/java/scw/data/redis/jedis/cluster/AbstractClusterOperations.java
+++ src/main/java/scw/data/redis/jedis/cluster/AbstractClusterOperations.java
@@ -1,11 +1,12 @@
 package scw.data.redis.jedis.cluster;
 
 import redis.clients.jedis.JedisCluster;
+import scw.data.redis.AbstractRedis;
 import scw.data.redis.Redis;
 import scw.data.redis.RedisOperations;
 import scw.data.redis.ResourceManager;
 
-public abstract class AbstractClusterOperations implements Redis, ResourceManager<JedisCluster> {
+public abstract class AbstractClusterOperations extends AbstractRedis implements Redis, ResourceManager<JedisCluster> {
 	private final RedisOperations<String, String> stringCommands = new AbstractClusterStringOperations() {
 
 		public JedisCluster getResource() {
@@ -26,6 +27,14 @@ public abstract class AbstractClusterOperations implements Redis, ResourceManage
 		public void close(JedisCluster resource) {
 			close(resource);
 		}
+
+		public long incr(byte[] key, long incr, long initValue) {
+			return getStringOperations().incr(new String(key, getCharset()), incr, initValue);
+		};
+
+		public long decr(byte[] key, long decr, long initValue) {
+			return getStringOperations().decr(new String(key, getCharset()), decr, initValue);
+		};
 	};
 
 	public RedisOperations<String, String> getStringOperations() {
