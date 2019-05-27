d282012ef22acf3af1dd40ca8a5229bf40771e71
wcnnkh <247044007@qq.com>
2019-05-27 17:51:10 +0800

    fix

--- src/main/java/scw/support/jxl/load/LoadRow.java
+++ src/main/java/scw/support/jxl/load/LoadRow.java
@@ -1,5 +1,5 @@
 package scw.support.jxl.load;
 
 public interface LoadRow {
-	public void load(int sheetIndex, int rowIndex, String[] contents);
+	void load(int sheetIndex, int rowIndex, String[] contents);
 }
