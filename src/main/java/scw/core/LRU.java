f7d5f0767126b6ded4bd066ce5cee506a5fd0b11
wcnnkh <247044007@qq.com>
2019-06-01 17:40:36 +0800

    fix

--- src/main/java/scw/core/LRU.java
+++
@@ -1,6 +0,0 @@
-package scw.core;
-
-import java.util.Map;
-
-public interface LRU<K, V> extends Map<K, V> {
-}
--- src/main/java/scw/core/LRULinkedHashMap.java
+++ src/main/java/scw/core/LRULinkedHashMap.java
@@ -4,12 +4,13 @@ import java.util.LinkedHashMap;
 
 /**
  * 线程不安全的
+ * 
  * @author shuchaowen
  *
  * @param <K>
  * @param <V>
  */
-public class LRULinkedHashMap<K, V> extends LinkedHashMap<K, V> implements LRU<K, V> {
+public class LRULinkedHashMap<K, V> extends LinkedHashMap<K, V> {
 	private static final long serialVersionUID = 1L;
 	private final int maxCapacity;
 
@@ -17,16 +18,19 @@ public class LRULinkedHashMap<K, V> extends LinkedHashMap<K, V> implements LRU<K
 		this(maxCapacity, maxCapacity + 1, 1, accessOrder);
 	}
 
-	public LRULinkedHashMap(int maxCapacity, int initialCapacity, float loadFactor) {
+	public LRULinkedHashMap(int maxCapacity, int initialCapacity,
+			float loadFactor) {
 		this(maxCapacity, initialCapacity, loadFactor, false);
 	}
 
-	public LRULinkedHashMap(int maxCapacity, int initialCapacity, boolean accessOrder) {
+	public LRULinkedHashMap(int maxCapacity, int initialCapacity,
+			boolean accessOrder) {
 		super(initialCapacity, 0.75f, accessOrder);
 		this.maxCapacity = maxCapacity;
 	}
 
-	public LRULinkedHashMap(int maxCapacity, int initialCapacity, float loadFactor, boolean accessOrder) {
+	public LRULinkedHashMap(int maxCapacity, int initialCapacity,
+			float loadFactor, boolean accessOrder) {
 		super(initialCapacity, loadFactor, accessOrder);
 		this.maxCapacity = maxCapacity;
 	}
