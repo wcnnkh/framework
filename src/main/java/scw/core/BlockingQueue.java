db039738c71d7f6fe9fdf191b76d6d56fc97592b
wcnnkh <247044007@qq.com>
2019-06-05 16:58:11 +0800

    添加柱塞队列的接口

---
+++ src/main/java/scw/core/BlockingQueue.java
@@ -0,0 +1,23 @@
+package scw.core;
+
+/**
+ * 阻塞队列
+ * @author shuchaowen
+ *
+ * @param <E>
+ */
+public interface BlockingQueue<E> {
+	/**
+	 * 将指定元素插入此队列中，将等待可用的空间（如果有必要）。
+	 * 
+	 * @param e
+	 */
+	void put(E e) throws InterruptedException;
+
+	/**
+	 * 获取并移除此队列的头部，在元素变得可用之前一直等待（如果有必要）。
+	 * 
+	 * @return
+	 */
+	E take() throws InterruptedException;
+}
