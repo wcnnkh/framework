87919b140a09c51195d33fb275b727060f7daae3
wcnnkh <247044007@qq.com>
2019-08-30 21:02:29 +0800

    fix

--- src/main/java/scw/mvc/support/ParameterChannel.java
+++ src/main/java/scw/mvc/support/ParameterChannel.java
@@ -1,62 +1,13 @@
 package scw.mvc.support;
 
 import java.lang.reflect.Type;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 
+import scw.core.ValueFactory;
 import scw.mvc.Channel;
 
-public interface ParameterChannel extends Channel {
-	<T> T getBean(Type type);
+public interface ParameterChannel extends Channel, ValueFactory<String> {
 
-	<T> T getBean(String name);
+	Object getObject(Type type);
 
-	<T> T getObject(Type type);
-
-	String getString(String name);
-
-	BigInteger getBigInteger(String name);
-
-	BigDecimal getBigDecimal(String name);
-
-	Class<?> getClass(String name);
-
-	@SuppressWarnings("rawtypes")
-	Enum getEnum(String name, Class<? extends Enum> type);
-
-	<E> E[] getArray(String name, Class<E> type);
-
-	Object getObject(String name, Class<?> type);
-
-	Byte getByte(String name);
-
-	byte getByteValue(String name);
-
-	Short getShort(String name);
-
-	short getShortValue(String name);
-
-	Integer getInteger(String name);
-
-	int getIntValue(String name);
-
-	Long getLong(String name);
-
-	long getLongValue(String name);
-
-	Boolean getBoolean(String name);
-
-	boolean getBooleanValue(String name);
-
-	Float getFloat(String name);
-
-	float getFloatValue(String name);
-
-	Double getDouble(String name);
-
-	double getDoubleValue(String name);
-
-	char getChar(String name);
-
-	Character getCharacter(String name);
+	Object getObject(Class<?> type);
 }
