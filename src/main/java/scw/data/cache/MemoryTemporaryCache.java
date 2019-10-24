a91b2f0dd149cc24eb67aa1d5c7b1266022ddfe1
wcnnkh <247044007@qq.com>
2019-10-24 23:00:15 +0800

    fix-cache

--- src/main/java/scw/data/cache/MemoryTemporaryCache.java
+++ src/main/java/scw/data/cache/MemoryTemporaryCache.java
@@ -1,14 +1,18 @@
 package scw.data.cache;
 
-import java.util.Map;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 import scw.core.Destroy;
+import scw.core.utils.StringUtils;
+import scw.core.utils.SystemPropertyUtils;
+import scw.core.utils.XTime;
 import scw.logger.Logger;
 import scw.logger.LoggerFactory;
-import scw.timer.support.RunnableTimerTask;
 
 /**
  * 内存实现
@@ -17,23 +21,42 @@ import scw.timer.support.RunnableTimerTask;
  *
  */
 public final class MemoryTemporaryCache implements TemporaryCache, Destroy {
-	private static Logger logger = LoggerFactory.getLogger(MemoryTemporaryCache.class);
+	private static final long DEFAULT_CLEAR_PERIOD = StringUtils.parseLong(
+			SystemPropertyUtils
+					.getProperty("memory.temporary.cache.clear.period"),
+			XTime.ONE_MINUTE);
+	private static Logger logger = LoggerFactory
+			.getLogger(MemoryTemporaryCache.class);
 	private Timer timer = new Timer(getClass().getSimpleName());
-	private volatile Map<String, TemporaryCacheContent> map = new ConcurrentHashMap<String, TemporaryCacheContent>();
+	private volatile ConcurrentHashMap<String, CacheContent> map = new ConcurrentHashMap<String, CacheContent>();
 
 	public MemoryTemporaryCache() {
+		timer.schedule(new ClearExpireKeyTask(), DEFAULT_CLEAR_PERIOD,
+				DEFAULT_CLEAR_PERIOD);
 	}
 
 	@SuppressWarnings("unchecked")
 	public <T> T get(String key) {
-		TemporaryCacheContent content = map.get(key);
-		return (T) (content == null ? null : content.getValue());
+		CacheContent content = map.get(key);
+		if (content == null) {
+			return null;
+		}
+
+		if (content.isExpire(System.currentTimeMillis())) {
+			return null;
+		}
+
+		return (T) content.getValue();
 	}
 
 	@SuppressWarnings("unchecked")
 	public <T> T getAndTouch(String key, int exp) {
-		TemporaryCacheContent content = map.get(key);
+		CacheContent content = map.get(key);
 		if (content == null) {
+			return null;
+		}
+
+		if (content.isExpire(System.currentTimeMillis())) {
 			return null;
 		}
 
@@ -42,7 +65,7 @@ public final class MemoryTemporaryCache implements TemporaryCache, Destroy {
 	}
 
 	public void touch(String key, int exp) {
-		TemporaryCacheContent content = map.get(key);
+		CacheContent content = map.get(key);
 		if (content == null) {
 			return;
 		}
@@ -51,38 +74,48 @@ public final class MemoryTemporaryCache implements TemporaryCache, Destroy {
 	}
 
 	public void delete(String key) {
-		TemporaryCacheContent content = map.remove(key);
-		if (content == null) {
-			return;
-		}
-
-		content.cancel();
+		map.remove(key);
 	}
 
 	public void set(String key, int exp, Object value) {
-		TemporaryCacheContent content = new TemporaryCacheContent(key, value);
-		TemporaryCacheContent oldContent = map.putIfAbsent(key, content);
+		CacheContent content = new CacheContent(value, exp);
+		CacheContent oldContent = map.putIfAbsent(key, content);
 		if (oldContent != null) {
 			oldContent.setValue(value);
 			oldContent.touch(exp);
-		} else {
-			content.touch(exp);
 		}
 	}
 
-	private final class TemporaryCacheContent implements Runnable {
-		private final String key;
-		private Object value;
-		private volatile TimerTask timerTask;
+	private final class ClearExpireKeyTask extends TimerTask {
 
-		public TemporaryCacheContent(String key, Object value) {
-			this.key = key;
-			this.value = value;
-		}
-
+		@Override
 		public void run() {
-			logger.info("Deleting expired key:{}", key);
-			map.remove(key);
+			try {
+				long currentTime = scheduledExecutionTime();
+				Iterator<Entry<String, CacheContent>> iterator = map.entrySet()
+						.iterator();
+				while (iterator.hasNext()) {
+					Entry<String, CacheContent> entry = iterator.next();
+					if (entry.getValue().isExpire(currentTime)) {
+						logger.debug("Deleting expired key:{}", entry.getKey());
+						iterator.remove();
+					}
+				}
+			} catch (Exception e) {
+				logger.error(e, "clear expired key error");
+			}
+		}
+	}
+
+	private final class CacheContent {
+		private Object value;
+		private final AtomicLong lastTouch;
+		private int exp;
+
+		public CacheContent(Object value, int exp) {
+			this.lastTouch = new AtomicLong(System.currentTimeMillis());
+			this.value = value;
+			this.exp = exp;
 		}
 
 		public Object getValue() {
@@ -93,18 +126,17 @@ public final class MemoryTemporaryCache implements TemporaryCache, Destroy {
 			this.value = value;
 		}
 
-		public void cancel() {
-			if (timerTask != null) {
-				timerTask.cancel();
-			}
+		public void touch(int exp) {
+			this.exp = exp;
+			this.lastTouch.set(System.currentTimeMillis());
 		}
 
-		public void touch(int exp) {
-			cancel();
-			if (exp > 0) {
-				this.timerTask = new RunnableTimerTask(this);
-				timer.schedule(timerTask, exp * 1000);
+		public boolean isExpire(long currentTimeMillis) {
+			if (exp <= 0) {
+				return false;
 			}
+
+			return currentTimeMillis - lastTouch.get() > exp * 1000L;
 		}
 	}
 
