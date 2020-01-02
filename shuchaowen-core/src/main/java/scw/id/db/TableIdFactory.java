84d6738eec20938c93fd30f77091c73471fd0b56
wcnnkh <247044007@qq.com>
2020-01-02 15:03:24 +0800

    remove TableIdFactory

--- shuchaowen-core/src/main/java/scw/id/db/TableIdFactory.java
+++
@@ -1,8 +0,0 @@
-package scw.id.db;
-
-import scw.beans.annotation.AutoImpl;
-
-@AutoImpl({MemcachedTableIdFactory.class, RedisTableIdFactory.class})
-public interface TableIdFactory {
-	long generator(Class<?> tableClass, String fieldName);
-}
