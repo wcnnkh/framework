2e069daa2c72c69b6122e85127201eb3b6a5693a
wcnnkh <247044007@qq.com>
2020-01-17 18:28:42 +0800

    fix

--- shuchaowen-mail/src/main/java/scw/mail/SessionBuilder.java
+++ shuchaowen-mail/src/main/java/scw/mail/SessionBuilder.java
@@ -7,57 +7,62 @@ import javax.mail.Session;
 public class SessionBuilder {
 	private String transportProtocol;
 	private String host;
-	private String port;
+	private int port;
 	private boolean auth;
 	private boolean sslEnable;
 	private boolean debug;
 
-	public String getTransportProtocol() {
-		return transportProtocol;
+	public SessionBuilder(String transportProtocol) {
+		this.transportProtocol = transportProtocol;
 	}
 
-	public void setTransportProtocol(String transportProtocol) {
-		this.transportProtocol = transportProtocol;
+	public final String getTransportProtocol() {
+		return transportProtocol;
 	}
 
 	public String getHost() {
 		return host;
 	}
 
-	public void setHost(String host) {
+	public SessionBuilder setHost(String host) {
 		this.host = host;
+		return this;
 	}
 
-	public String getPort() {
+	public int getPort() {
 		return port;
 	}
 
-	public void setPort(String port) {
+	public SessionBuilder setPort(int port) {
 		this.port = port;
+		return this;
 	}
 
 	public boolean isAuth() {
 		return auth;
 	}
 
-	public void setAuth(boolean auth) {
+	public SessionBuilder setAuth(boolean auth) {
 		this.auth = auth;
+		return this;
 	}
 
 	public boolean isSslEnable() {
 		return sslEnable;
 	}
 
-	public void setSslEnable(boolean sslEnable) {
+	public SessionBuilder setSslEnable(boolean sslEnable) {
 		this.sslEnable = sslEnable;
+		return this;
 	}
 
 	public boolean isDebug() {
 		return debug;
 	}
 
-	public void setDebug(boolean debug) {
+	public SessionBuilder setDebug(boolean debug) {
 		this.debug = debug;
+		return this;
 	}
 
 	private String toPropertyKey(String suffix) {
@@ -69,9 +74,9 @@ public class SessionBuilder {
 		properties.put("mail.transport.protocol", getTransportProtocol().toLowerCase());// 连接协议
 		properties.put(toPropertyKey("host"), getHost());// 主机名
 		properties.put(toPropertyKey("port"), getPort());// 端口号
-		properties.put(toPropertyKey("auth"), "true");
-		properties.put(toPropertyKey("ssl.enable"), "true");// 设置是否使用ssl安全连接
-															// ---一般都使用
+		properties.put(toPropertyKey("auth"), isAuth() + "");
+		properties.put(toPropertyKey("ssl.enable"), isSslEnable() + "");// 设置是否使用ssl安全连接
+		// ---一般都使用
 		properties.put("mail.debug", "true");// 设置是否显示debug信息 true 会在控制台显示相关信息
 		return properties;
 	}
@@ -82,5 +87,10 @@ public class SessionBuilder {
 			session.setDebug(isDebug());
 		}
 		return session;
+	}
+
+	public static Session builderDefault(boolean debug) {
+		return new SessionBuilder("smtp").setSslEnable(true).setAuth(true).setHost("smtp.qq.com").setDebug(debug)
+				.setPort(465).builder();
 	}
 }
