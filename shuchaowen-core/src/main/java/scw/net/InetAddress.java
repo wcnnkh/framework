14a4848bcfdb436cbfd9b3bd6f433dfa626336d7
wcnnkh <247044007@qq.com>
2020-05-29 15:46:17 +0800

    Cleanup

--- shuchaowen-core/src/main/java/scw/net/InetAddress.java
+++ shuchaowen-core/src/main/java/scw/net/InetAddress.java
@@ -3,8 +3,6 @@ package scw.net;
 import java.io.Serializable;
 import java.net.InetSocketAddress;
 
-import scw.core.utils.StringUtils;
-
 /**
  * 使用此类的原因是{@see InetSocketAddress#getHostName()}会反查dns
  * 
@@ -15,10 +13,16 @@ public class InetAddress implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private final String host;
 	private final int port;
+	private InetSocketAddress inetSocketAddress;
 
 	public InetAddress(String host, int port) {
+		this(host, port, new InetSocketAddress(host, port));
+	}
+
+	public InetAddress(String host, int port, InetSocketAddress inetSocketAddress) {
 		this.host = host;
 		this.port = port;
+		this.inetSocketAddress = inetSocketAddress;
 	}
 
 	public String getHost() {
@@ -29,11 +33,7 @@ public class InetAddress implements Serializable {
 		return port;
 	}
 
-	public InetSocketAddress toInetSocketAddress() {
-		String host = getHost();
-		if (StringUtils.isEmpty(host)) {
-			return new InetSocketAddress(getPort());
-		}
-		return new InetSocketAddress(host, getPort());
+	public InetSocketAddress getInetSocketAddress() {
+		return inetSocketAddress;
 	}
 }
