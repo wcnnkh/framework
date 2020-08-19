97a91792868df32d8a595e1ece45dc292754f1e8
wcnnkh <247044007@qq.com>
2020-08-19 18:11:09 +0800

    支持http multipart

---
+++ shuchaowen-core/src/main/java/scw/http/multipart/DefaultFileItem.java
@@ -0,0 +1,59 @@
+package scw.http.multipart;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import scw.http.ContentDisposition;
+import scw.http.MediaType;
+import scw.net.FileMimeTypeUitls;
+import scw.net.MimeType;
+
+class DefaultFileItem extends FileItem {
+	private final File file;
+	private final boolean temporaryFile;
+
+	public DefaultFileItem(String fieldName, File file) {
+		this(fieldName, file, false);
+	}
+
+	public DefaultFileItem(String fieldName, File file, boolean temporaryFile) {
+		super(fieldName);
+		this.file = file;
+		this.temporaryFile = temporaryFile;
+		ContentDisposition contentDisposition = ContentDisposition.builder("form-data").name(fieldName)
+				.filename(file.getName()).build();
+		getHeaders().setContentDisposition(contentDisposition);
+		MimeType mimeType = FileMimeTypeUitls.getMimeType(file.getName());
+		if (mimeType != null) {
+			getHeaders().setContentType(new MediaType(mimeType));
+		}
+		getHeaders().setContentLength(file.length());
+	}
+
+	public boolean isTemporaryFile() {
+		return temporaryFile;
+	}
+
+	public void close() throws IOException {
+		if (isTemporaryFile()) {
+			file.delete();
+		}
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		close();
+		super.finalize();
+	}
+
+	public InputStream getBody() throws IOException {
+		return new FileInputStream(file);
+	}
+	
+	@Override
+	public String getName() {
+		return file.getName();
+	}
+}
---
+++ shuchaowen-core/src/main/java/scw/http/multipart/FileItem.java
@@ -0,0 +1,36 @@
+package scw.http.multipart;
+
+import java.io.Closeable;
+
+import scw.http.AbstractHttpInputMessage;
+import scw.http.HttpHeaders;
+import scw.http.HttpInputMessage;
+
+public abstract class FileItem extends AbstractHttpInputMessage implements HttpInputMessage, Closeable {
+	private final HttpHeaders httpHeaders = new HttpHeaders();
+	private final String fieldName;
+
+	public FileItem(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public HttpHeaders getHeaders() {
+		return httpHeaders;
+	}
+
+	/**
+	 * 获取文件名
+	 * @return
+	 */
+	public String getName() {
+		return null;
+	}
+
+	/**
+	 * 字段名
+	 * @return
+	 */
+	public String getFieldName() {
+		return fieldName;
+	}
+}
