8da957df444aa5f15abd4c79fec482875f272e6f
wcnnkh <247044007@qq.com>
2020-09-04 18:01:20 +0800

    Cleanup

--- scw-core/src/main/java/scw/aop/ProxyFactory.java
+++ scw-core/src/main/java/scw/aop/ProxyFactory.java
@@ -15,7 +15,7 @@ public interface ProxyFactory {
 	 */
 	boolean isSupport(Class<?> clazz);
 
-	Proxy getProxy(Class<?> clazz, Class<?>[] interfaces, Iterable<MethodInterceptor> filters);
+	Proxy getProxy(Class<?> clazz, Class<?>[] interfaces, Iterable<? extends MethodInterceptor> filters);
 	
 	/**
 	 * 获取代理类
