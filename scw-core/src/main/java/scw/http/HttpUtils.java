3a30449f79a1fc4db2d370dfe5ef995c202be8a1
wcnnkh <247044007@qq.com>
2020-09-07 18:01:02 +0800

    Cleanup

--- scw-core/src/main/java/scw/http/HttpUtils.java
+++ scw-core/src/main/java/scw/http/HttpUtils.java
@@ -32,12 +32,12 @@ import scw.value.Value;
 public final class HttpUtils {
 	private HttpUtils() {
 	};
-	
+
 	private static final HttpClient HTTP_CLIENT = InstanceUtils.loadService(HttpClient.class,
 			"scw.http.client.SimpleHttpClient");
 	private static final ServerHttpRequestIpGetter SERVER_HTTP_REQUEST_IP_GETTER = InstanceUtils
 			.loadService(ServerHttpRequestIpGetter.class, "scw.http.server.ip.DefaultServerHttpRequestIpGetter");
-	
+
 	static {
 
 	}
@@ -76,25 +76,26 @@ public final class HttpUtils {
 		if (origin == null) {
 			return true;
 		}
-		
+
 		return isSameOrigin(request.getURI(), UriComponentsBuilder.fromOriginHeader(origin).build().toUri());
 	}
-	
+
 	/**
 	 * 判断两个url是否同源
+	 * 
 	 * @param url1
 	 * @param url2
 	 * @return
 	 */
-	public static boolean isSameOrigin(String url1, String url2){
-		if(url1 == null || url2 == null){
+	public static boolean isSameOrigin(String url1, String url2) {
+		if (url1 == null || url2 == null) {
 			return false;
 		}
-		
-		if(StringUtils.equals(url1, url2)){
+
+		if (StringUtils.equals(url1, url2)) {
 			return true;
 		}
-		
+
 		try {
 			return isSameOrigin(new URI(url1), new URI(url2));
 		} catch (URISyntaxException e) {
@@ -104,19 +105,20 @@ public final class HttpUtils {
 
 	/**
 	 * 判断两个uri是否同源
+	 * 
 	 * @param uri1
 	 * @param uri2
 	 * @return
 	 */
 	public static boolean isSameOrigin(URI uri1, URI uri2) {
-		if(uri1 == null || uri2 == null){
+		if (uri1 == null || uri2 == null) {
 			return false;
 		}
-		
-		if(uri1.equals(uri2)){
+
+		if (uri1.equals(uri2)) {
 			return true;
 		}
-		
+
 		return (ObjectUtils.nullSafeEquals(uri1.getScheme(), uri2.getScheme())
 				&& ObjectUtils.nullSafeEquals(uri1.getHost(), uri2.getHost())
 				&& getPort(uri1.getScheme(), uri1.getPort()) == getPort(uri2.getScheme(), uri2.getPort()));
@@ -163,9 +165,10 @@ public final class HttpUtils {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 将文件信息写入ContentDisposition
+	 * 
 	 * @param outputMessage
 	 * @param fileName
 	 */
@@ -178,87 +181,110 @@ public final class HttpUtils {
 				.filename(fileName, Constants.UTF_8).build();
 		outputMessage.getHeaders().setContentDisposition(contentDisposition);
 	}
-	
+
+	/**
+	 * 缓存是否过期,如果未过期那么返回304，如果已过期则setLastModified
+	 * 
+	 * @param request
+	 * @param response
+	 * @param lastModified
+	 * @return
+	 */
+	public static boolean isExpired(ServerHttpRequest request, ServerHttpResponse response, long lastModified) {
+		response.getHeaders().setLastModified(lastModified);
+		long ifModifiedSince = request.getHeaders().getIfModifiedSince();
+		if (ifModifiedSince < 0 || lastModified < 0) {
+			// 缓存已过期,请求中没有此值
+			return true;
+		}
+
+		// 不比较毫秒
+		if (ifModifiedSince / 1000 != lastModified / 1000) {
+			// 缓存已过期
+			return true;
+		}
+
+		// 客户端缓存未过期
+		response.setStatusCode(HttpStatus.NOT_MODIFIED);
+		return false;
+	}
+
 	/**
 	 * 写入一个静态资源
+	 * 
 	 * @param request
 	 * @param response
 	 * @param resource
 	 * @param mimeType
 	 * @throws IOException
 	 */
-	public static void writeStaticResource(ServerHttpRequest request, ServerHttpResponse response, Resource resource, MimeType mimeType) throws IOException{
-		if(!resource.exists()){
+	public static void writeStaticResource(ServerHttpRequest request, ServerHttpResponse response, Resource resource,
+			MimeType mimeType) throws IOException {
+		if (!resource.exists()) {
 			response.sendError(HttpStatus.NOT_FOUND.value(), "The resource does not exist!");
-			return ;
+			return;
 		}
-		
+
 		if (mimeType != null) {
 			response.setContentType(mimeType);
 		}
-		
-		long ifModifiedSince = request.getHeaders().getIfModifiedSince();
-		long lastModified = resource.lastModified();
-		if(lastModified <= ifModifiedSince){
-			//客户端缓存未过期
-			response.setStatusCode(HttpStatus.NOT_MODIFIED);
-			return ;
-		}
-		
-		if(lastModified > 0){
-			response.getHeaders().setLastModified(lastModified);
+
+		if (!isExpired(request, response, resource.lastModified())) {
+			return;
 		}
 		IOUtils.copy(resource.getInputStream(), response.getBody());
 	}
-	
+
 	/**
 	 * 根据参数名获取
+	 * 
 	 * @param request
 	 * @param name
 	 * @return 如果不存在返回{@see EmptyValue}
 	 * @throws IOException
 	 */
-	public static Value getParameter(ServerHttpRequest request, String name){
+	public static Value getParameter(ServerHttpRequest request, String name) {
 		String value = request.getParameterMap().getFirst(name);
-		if(value != null){
+		if (value != null) {
 			return new StringValue(value);
 		}
-		
-		if(request instanceof JsonServerHttpRequest){
+
+		if (request instanceof JsonServerHttpRequest) {
 			JsonObject jsonObject = ((JsonServerHttpRequest) request).getJsonObject();
-			if(jsonObject != null){
+			if (jsonObject != null) {
 				return jsonObject.get(name);
 			}
 		}
 		return EmptyValue.INSTANCE;
 	}
-	
+
 	/**
 	 * 此方法不会返回空，如果不存在返回的数组长度为0
+	 * 
 	 * @param request
 	 * @param name
 	 * @return
 	 */
-	public static Value[] getParameterValues(ServerHttpRequest request, String name){
+	public static Value[] getParameterValues(ServerHttpRequest request, String name) {
 		List<String> valueList = request.getParameterMap().get(name);
-		if(!CollectionUtils.isEmpty(valueList)){
+		if (!CollectionUtils.isEmpty(valueList)) {
 			Value[] values = new Value[valueList.size()];
 			int index = 0;
-			for(String value : valueList){
+			for (String value : valueList) {
 				values[index++] = new StringValue(value);
 			}
 			return values;
 		}
-		
-		if(request instanceof JsonServerHttpRequest){
+
+		if (request instanceof JsonServerHttpRequest) {
 			JsonObject jsonObject = ((JsonServerHttpRequest) request).getJsonObject();
-			if(jsonObject != null){
+			if (jsonObject != null) {
 				JsonElement jsonElement = jsonObject.get(name);
-				if(jsonElement.isJsonArray()){
+				if (jsonElement.isJsonArray()) {
 					JsonArray jsonArray = jsonElement.getAsJsonArray();
 					Value[] values = new Value[jsonArray.size()];
 					int index = 0;
-					for(JsonElement element : jsonElement.getAsJsonArray()){
+					for (JsonElement element : jsonElement.getAsJsonArray()) {
 						values[index++] = element;
 					}
 					return values;
