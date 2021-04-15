63d36582e7a4955b15af4894adf92261d7431ab3
shuchaowen <35984@LAPTOP-EO2I0V8F>
2021-04-15 10:48:10 +0800

    Cleanup

--- scw-core/src/main/java/scw/logger/LoggerFactory.java
+++ scw-core/src/main/java/scw/logger/LoggerFactory.java
@@ -7,6 +7,7 @@ import java.util.ServiceLoader;
 import java.util.logging.Handler;
 
 import scw.core.utils.CollectionUtils;
+import scw.lang.Nullable;
 
 public final class LoggerFactory {
 	private static final java.util.logging.Logger ROOT_LOGGER = java.util.logging.Logger
@@ -27,23 +28,43 @@ public final class LoggerFactory {
 			}
 		}
 
-		// 是否存在第三方日志系统
-		if (LOGGER_FACTORY == null) {
-
-		} else {
-			ROOT_LOGGER.info("Use logger factory [" + LOGGER_FACTORY + "]");
+		//存在第三方日志系统
+		if(LOGGER_FACTORY != null) {
+			Logger logger = LOGGER_FACTORY.getLogger(LoggerFactory.class.getName());
+			logger.info("Use logger factory [" + LOGGER_FACTORY + "]");
 		}
 	}
 
 	/**
-	 * 获取根日志记录器
-	 * 
+	 * 获取根日志记录器<br/>
+	 * 一般不使用此记录器进行输出，除非你知道你想做什么
+	 * @see #getLogger(String)
+	 * @see #getLogger(Class)
 	 * @return
 	 */
 	public static java.util.logging.Logger getRootLogger() {
 		return ROOT_LOGGER;
 	}
 
+	/**
+	 * 获取默认日志工厂实现<br/>
+	 * 一般情况下不要使用此访求获取日志记录器，除非你知道你想做什么
+	 * @see #getLogger(String)
+	 * @see #getLogger(Class)
+	 * @return
+	 */
+	@Nullable
+	public static ILoggerFactory getLoggerFactory() {
+		return LOGGER_FACTORY;
+	}
+	
+	/**
+	 * 获取一个日志记录器
+	 * @see ILoggerFactory
+	 * @see Handler
+	 * @param name
+	 * @return
+	 */
 	public static Logger getLogger(String name) {
 		Logger cacheLogger = loggerMap.get(name);
 		if(cacheLogger == null) {
@@ -60,7 +81,6 @@ public final class LoggerFactory {
 					} else {
 						cacheLogger = LOGGER_FACTORY.getLogger(name);
 					}
-					
 					loggerMap.put(name, cacheLogger);
 				}
 			}
@@ -68,6 +88,12 @@ public final class LoggerFactory {
 		return cacheLogger;
 	}
 
+	/**
+	 * 获取一个日志记录器
+	 * @see #getLogger(String)
+	 * @param clazz
+	 * @return
+	 */
 	public static Logger getLogger(Class<?> clazz) {
 		return getLogger(clazz.getName());
 	}
