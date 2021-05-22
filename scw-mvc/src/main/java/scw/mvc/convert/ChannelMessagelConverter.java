9386d6efecb98bf82cab1be0c00b238690a99361
wcnnkh <247044007@qq.com>
2021-05-22 23:07:49 +0800

    cleanup

---
+++ scw-mvc/src/main/java/scw/mvc/convert/ChannelMessagelConverter.java
@@ -0,0 +1,22 @@
+package scw.mvc.convert;
+
+import java.io.IOException;
+
+import scw.convert.TypeDescriptor;
+import scw.core.parameter.ParameterDescriptor;
+import scw.http.server.ServerHttpRequest;
+import scw.http.server.ServerHttpResponse;
+
+public interface ChannelMessagelConverter {
+	boolean canRead(ParameterDescriptor parameterDescriptor);
+
+	Object read(ParameterDescriptor parameterDescriptor,
+			ServerHttpRequest request) throws IOException,
+			ChannelMessagelConverterException;
+
+	boolean canWrite(TypeDescriptor type, Object body);
+
+	void write(TypeDescriptor type, Object body, ServerHttpRequest request,
+			ServerHttpResponse response) throws IOException,
+			ChannelMessagelConverterException;
+}
