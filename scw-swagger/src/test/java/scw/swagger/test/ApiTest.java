04574d7a5e6f280064332c332b850e9fd675ea7c
wcnnkh <247044007@qq.com>
2021-06-26 22:55:14 +0800

    cleanup

--- scw-swagger/src/test/java/scw/swagger/test/ApiTest.java
+++ scw-swagger/src/test/java/scw/swagger/test/ApiTest.java
@@ -1,16 +1,25 @@
 package scw.swagger.test;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 @Path("abc")
+@Tag(name="用户信息", description="用于获取用户基本信息")
 public class ApiTest {
 	@GET
-	@Tag(description = "这是描述啊", name = "测试一下")
 	@Path("/gef")
+	@Operation(description="用于获取名称", summary="获取名称", method="methodaaadasdfasdf")
 	public String a() {
+		return "";
+	}
+	
+	@GET
+	@Operation(description="获取手机号")
+	@Path("/def")
+	public String b() {
 		return "";
 	}
 }
--- scw-swagger/src/test/java/scw/swagger/test/SwaggerMain.java
+++ scw-swagger/src/test/java/scw/swagger/test/SwaggerMain.java
@@ -1,9 +1,13 @@
 package scw.swagger.test;
 
 import io.swagger.v3.oas.integration.OpenApiConfigurationException;
+
+import org.junit.Test;
+
 import scw.boot.support.MainApplication;
 
 public class SwaggerMain {
+	@Test
 	public void test() throws OpenApiConfigurationException, InterruptedException {
 		MainApplication.run(SwaggerMain.class);
 		/*
