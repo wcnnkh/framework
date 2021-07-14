cb82daff49e500e04e6548db9ca7ca1d341cc150
舒超文 <shuchaowen@zx62580.com>
2021-07-14 22:03:24 +0800

    cleanup

--- scw-ibatis/src/test/java/scw/ibatis/test/IbatisTest.java
+++ scw-ibatis/src/test/java/scw/ibatis/test/IbatisTest.java
@@ -2,11 +2,14 @@ package scw.ibatis.test;
 
 import java.util.concurrent.ExecutionException;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+
 import scw.boot.Application;
 import scw.boot.support.MainApplication;
 
 public class IbatisTest {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		Application application = MainApplication.run(IbatisTest.class, args).get();
+		SqlSessionFactory sqlSessionFactory = application.getInstance(SqlSessionFactory.class);
 	}
 }
