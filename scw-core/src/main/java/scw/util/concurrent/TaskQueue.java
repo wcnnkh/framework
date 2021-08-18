719b08934345ce2c93421700a506995e1cfb0db8
舒超文 <shuchaowen@zx62580.com>
2021-08-18 10:37:47 +0800

    cleanup

--- scw-core/src/main/java/scw/util/concurrent/TaskQueue.java
+++ scw-core/src/main/java/scw/util/concurrent/TaskQueue.java
@@ -7,9 +7,11 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionException;
 
+import scw.core.Assert;
 import scw.lang.Nullable;
 import scw.logger.Logger;
 import scw.logger.LoggerFactory;
+import scw.util.XUtils;
 
 /**
  * 任务队列 默认是一个守护线程自动退出
@@ -56,18 +58,30 @@ public class TaskQueue extends Thread implements AsyncExecutor {
 			try {
 				task.run();
 			} catch (Throwable e) {
-				logger.error(e, "task:{}", task);
+				logger.error(e, "fail: {}", task);
 			}
 		}
 	}
 
 	@Override
 	public <T> Future<T> submit(Callable<T> task) {
+		Assert.requiredArgument(task != null, "task");
 		if(isInterrupted()){
 			throw new RejectedExecutionException(String.valueOf(task));
 		}
 		
-		FutureTask<T> future = new FutureTask<T>(task);
+		final String taskId = XUtils.getUUID();
+		FutureTask<T> future = new FutureTask<T>(task) {
+			@Override
+			public String toString() {
+				return taskId + "[" + task.toString() + "]";
+			}
+		};
+		
+		if(logger.isDebugEnabled()) {
+			logger.debug("submit: {}", future);
+		}
+		
 		if (!queue.offer(future)) {
 			throw new RejectedExecutionException(String.valueOf(task));
 		}
