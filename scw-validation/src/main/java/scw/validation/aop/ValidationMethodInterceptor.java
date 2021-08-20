4b49fa4ca9b2a63e65f7750c1aa0054dc52aa6d4
舒超文 <shuchaowen@zx62580.com>
2021-08-20 14:19:53 +0800

    cleanup

--- scw-validation/src/main/java/scw/validation/aop/ValidationMethodInterceptor.java
+++ scw-validation/src/main/java/scw/validation/aop/ValidationMethodInterceptor.java
@@ -1,15 +1,10 @@
 package scw.validation.aop;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 
 import scw.aop.MethodInterceptor;
 import scw.context.annotation.Provider;
 import scw.core.reflect.MethodInvoker;
-import scw.core.utils.CollectionUtils;
 import scw.validation.FastValidator;
 
 @Provider
@@ -26,19 +21,11 @@ public class ValidationMethodInterceptor implements MethodInterceptor {
 
 	@Override
 	public Object intercept(MethodInvoker invoker, Object[] args) throws Throwable {
-		Set<ConstraintViolation<Object>> constraintViolations = validator.forExecutables()
-				.validateParameters(invoker.getInstance(), invoker.getMethod(), args);
-		if(!CollectionUtils.isEmpty(constraintViolations)) {
-			throw new ConstraintViolationException(constraintViolations);
-		}
-		
+		FastValidator.validate(() -> validator.forExecutables()
+				.validateParameters(invoker.getInstance(), invoker.getMethod(), args));
 		Object returnValue = invoker.invoke(args);
-		
-		constraintViolations = validator.forExecutables().validateReturnValue(invoker.getInstance(),
-				invoker.getMethod(), returnValue);
-		if(!CollectionUtils.isEmpty(constraintViolations)) {
-			throw new ConstraintViolationException(constraintViolations);
-		}
+		FastValidator.validate(() -> validator.forExecutables().validateReturnValue(invoker.getInstance(),
+				invoker.getMethod(), returnValue));
 		return returnValue;
 	}
 }
