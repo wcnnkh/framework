43358910095e410d5826f84ab4d3a9324b61d2b4
舒超文 <shuchaowen@zx62580.com>
2021-09-01 13:12:51 +0800

    cleanup

--- core/src/main/java/io/basc/framework/gson/GsonSupport.java
+++ core/src/main/java/io/basc/framework/gson/GsonSupport.java
@@ -1,9 +1,13 @@
 package io.basc.framework.gson;
 
+import java.io.IOException;
+import java.io.Reader;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import io.basc.framework.json.AbstractJSONSupport;
+import io.basc.framework.json.JSONException;
 import io.basc.framework.json.JsonElement;
 
 public final class GsonSupport extends AbstractJSONSupport {
@@ -27,7 +31,13 @@ public final class GsonSupport extends AbstractJSONSupport {
 		com.google.gson.JsonElement gsonJsonElement = gson.fromJson(text, com.google.gson.JsonElement.class);
 		return new GsonElement(gsonJsonElement, gson);
 	}
-
+	
+	@Override
+	public JsonElement parseJson(Reader reader) throws IOException, JSONException {
+		com.google.gson.JsonElement gsonJsonElement = gson.fromJson(reader, com.google.gson.JsonElement.class);
+		return new GsonElement(gsonJsonElement, gson);
+	}
+	
 	@Override
 	protected String toJsonStringInternal(Object obj) {
 		return gson.toJson(obj);
