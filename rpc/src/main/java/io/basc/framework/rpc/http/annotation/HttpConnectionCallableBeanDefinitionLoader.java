c663248abe7c75ec239248dec9ee01a97916ae88
wcnnkh <247044007@qq.com>
2021-09-26 22:14:21 +0800

    cleanup

--- rpc/src/main/java/io/basc/framework/rpc/http/annotation/HttpConnectionCallableBeanDefinitionLoader.java
+++ rpc/src/main/java/io/basc/framework/rpc/http/annotation/HttpConnectionCallableBeanDefinitionLoader.java
@@ -3,10 +3,12 @@ package io.basc.framework.rpc.http.annotation;
 import io.basc.framework.beans.BeanDefinition;
 import io.basc.framework.beans.BeanDefinitionLoader;
 import io.basc.framework.beans.BeanDefinitionLoaderChain;
+import io.basc.framework.beans.BeanFactory;
 import io.basc.framework.beans.ConfigurableBeanFactory;
 import io.basc.framework.context.annotation.Provider;
 import io.basc.framework.core.Ordered;
-import io.basc.framework.http.client.DefaultHttpClient;
+import io.basc.framework.http.HttpUtils;
+import io.basc.framework.http.client.HttpClient;
 import io.basc.framework.rpc.CallableFactory;
 import io.basc.framework.rpc.support.RemoteCallableBeanDefinition;
 
@@ -23,18 +25,24 @@ public class HttpConnectionCallableBeanDefinitionLoader implements BeanDefinitio
 			return loaderChain.load(beanFactory, sourceClass);
 		}
 		
-		DefaultHttpClient httpConnectionFactory = new DefaultHttpClient();
-		httpConnectionFactory.configure(beanFactory);
-		
+		HttpClient httpClient = getHttpClient(beanFactory);
 		CallableFactory callableFactory;
 		if (remote != null) {
-			callableFactory = new AnnotationHttpCallableFactory(httpConnectionFactory, remote,
+			callableFactory = new AnnotationHttpCallableFactory(httpClient, remote,
 					beanFactory.getDefaultValueFactory());
 		} else {
-			callableFactory = new AnnotationHttpCallableFactory(httpConnectionFactory, path,
+			callableFactory = new AnnotationHttpCallableFactory(httpClient, path,
 					beanFactory.getDefaultValueFactory());
 		}
 		return new RemoteCallableBeanDefinition(beanFactory, callableFactory, sourceClass);
+	}
+	
+	private HttpClient getHttpClient(BeanFactory beanFactory){
+		if(beanFactory.isInstance(HttpClient.class)){
+			return beanFactory.getInstance(HttpClient.class);
+		}else{
+			return HttpUtils.getHttpClient();
+		}
 	}
 
 }
