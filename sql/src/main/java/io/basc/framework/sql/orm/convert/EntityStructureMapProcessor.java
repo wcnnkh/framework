1d56e2764f8afe9f7cf394b9d026e42f73f3f5ab
舒超文 <shuchaowen@zx62580.com>
2021-10-28 17:52:50 +0800

    cleanup

--- sql/src/main/java/io/basc/framework/sql/orm/convert/EntityStructureMapProcessor.java
+++ sql/src/main/java/io/basc/framework/sql/orm/convert/EntityStructureMapProcessor.java
@@ -6,14 +6,16 @@ import io.basc.framework.mapper.Field;
 import io.basc.framework.orm.EntityStructure;
 import io.basc.framework.orm.Property;
 import io.basc.framework.sql.SqlUtils;
+import io.basc.framework.util.CollectionUtils;
 import io.basc.framework.util.MultiValueMap;
 import io.basc.framework.util.stream.Processor;
 
 import java.sql.ResultSet;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
-public class EntityStructureMapProcessor<T> implements
-		Processor<ResultSet, T, Throwable> {
+public class EntityStructureMapProcessor<T> implements Processor<ResultSet, T, Throwable> {
 	private final EntityStructure<? extends Property> structure;
 	private final ConversionService conversionService;
 
@@ -27,18 +29,39 @@ public class EntityStructureMapProcessor<T> implements
 		this.conversionService = conversionService;
 	}
 
+	public <V> V getValue(Map<String, V> valueMap, Property property) {
+		if (valueMap.containsKey(property.getName())) {
+			return valueMap.get(property.getName());
+		}
+
+		Collection<String> names = property.getAliasNames();
+		if (CollectionUtils.isEmpty(names)) {
+			return null;
+		}
+
+		for (String name : names) {
+			if (valueMap.containsKey(name)) {
+				return valueMap.get(name);
+			}
+		}
+		return null;
+	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public T process(ResultSet rs) throws Throwable {
 		MultiValueMap<String, Object> valueMap = SqlUtils.getRowValueMap(rs);
 		Object instance = Sys.env.getInstance(structure.getEntityClass());
 		for (Property column : structure) {
-			if (valueMap.containsKey(column.getName())) {
-				Field field = column.getField();
-				List<Object> columnValues = valueMap.get(column.getName());
-				Object columnValue = columnValues.size() == 1 ? columnValues.get(0) : columnValues;
-				field.set(instance, columnValue, conversionService);
+			Object value = getValue(valueMap, column);
+			if (value == null) {
+				continue;
 			}
+
+			Field field = column.getField();
+			List<Object> columnValues = valueMap.get(column.getName());
+			Object columnValue = columnValues.size() == 1 ? columnValues.get(0) : columnValues;
+			field.set(instance, columnValue, conversionService);
 		}
 		return (T) instance;
 	}
--- sql/src/main/java/io/basc/framework/sql/orm/support/AbstractTableResolverExtend.java
+++ sql/src/main/java/io/basc/framework/sql/orm/support/AbstractTableResolverExtend.java
@@ -2,37 +2,25 @@ package io.basc.framework.sql.orm.support;
 
 import java.util.Collection;
 
-import io.basc.framework.core.annotation.AnnotatedElementUtils;
 import io.basc.framework.mapper.FieldDescriptor;
 import io.basc.framework.orm.support.AbstractObjectRelationalExtend;
 import io.basc.framework.sql.orm.IndexInfo;
 import io.basc.framework.sql.orm.TableResolver;
-import io.basc.framework.sql.orm.annotation.Table;
-import io.basc.framework.util.StringUtils;
 
 public class AbstractTableResolverExtend extends AbstractObjectRelationalExtend implements TableResolverExtend {
 
 	@Override
 	public Collection<IndexInfo> getIndexs(Class<?> entityClass, FieldDescriptor descriptor, TableResolver chain) {
-		// TODO Auto-generated method stub
-		return null;
+		return chain.getIndexs(entityClass, descriptor);
 	}
 
 	@Override
 	public String getEngine(Class<?> entityClass, TableResolver chain) {
-		Table table = AnnotatedElementUtils.getMergedAnnotation(entityClass, Table.class);
-		if (table != null && StringUtils.hasText(table.engine())) {
-			return table.engine();
-		}
 		return chain.getEngine(entityClass);
 	}
 
 	@Override
 	public String getRowFormat(Class<?> entityClass, TableResolver chain) {
-		Table table = AnnotatedElementUtils.getMergedAnnotation(entityClass, Table.class);
-		if (table != null && StringUtils.hasText(table.rowFormat())) {
-			return table.rowFormat();
-		}
 		return chain.getRowFormat(entityClass);
 	}
 
