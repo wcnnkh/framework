923ea5a5d0c4cc790b9fd0cfa27dff8fe8fab647
wcnnkh <alisa@192.168.1.100>
2021-12-27 22:10:46 +0800

    cleanup

--- rpc/src/test/java/io/basc/framework/rpc/test/HttpRemoteTest.java
+++ rpc/src/test/java/io/basc/framework/rpc/test/HttpRemoteTest.java
@@ -9,19 +9,18 @@ import io.basc.framework.boot.support.MainApplication;
 import io.basc.framework.logger.Levels;
 import io.basc.framework.logger.LoggerFactory;
 import io.basc.framework.net.InetUtils;
-import io.basc.framework.rpc.http.annotation.HttpRemote;
 import io.basc.framework.util.XUtils;
 import io.basc.framework.web.pattern.annotation.RequestMapping;
 
 @RequestMapping
-public class HttRemoteTest {
+public class HttpRemoteTest {
 	private static String index = XUtils.getUUID();
 
 	@Test
 	public void test() throws Throwable {
 		LoggerFactory.getLevelManager().getCustomLevelRegistry().put("io.basc.framework.rpc", Levels.DEBUG.getValue());
 		Application application = MainApplication
-				.run(HttpRemote.class, new String[] { "-p", InetUtils.getAvailablePort() + "" }).get();
+				.run(HttpRemoteTest.class, new String[] { "-p", InetUtils.getAvailablePort() + "" }).get();
 		TestRemoteInterface test = application.getInstance(TestRemoteInterface.class);
 		String response = test.index();
 		System.out.println(response);
--- rpc/src/test/java/io/basc/framework/rpc/test/TestRemoteInterface.java
+++ rpc/src/test/java/io/basc/framework/rpc/test/TestRemoteInterface.java
@@ -3,7 +3,7 @@ package io.basc.framework.rpc.test;
 import io.basc.framework.rpc.http.annotation.HttpRemote;
 import io.basc.framework.web.pattern.annotation.RequestMapping;
 
-@HttpRemote("http://localhost:${server.port}")
+@HttpRemote("http://127.0.0.1:${server.port}")
 public interface TestRemoteInterface {
 	@RequestMapping
 	String index();
