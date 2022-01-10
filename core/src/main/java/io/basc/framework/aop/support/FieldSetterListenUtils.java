dc07e1c14feaee23ece5b1376eef21454a9e6573
舒超文 <shuchaowen@zx62580.com>
2022-01-10 16:05:05 +0800

    cleanup

--- core/src/main/java/io/basc/framework/aop/support/FieldSetterListenUtils.java
+++ core/src/main/java/io/basc/framework/aop/support/FieldSetterListenUtils.java
@@ -1,5 +1,8 @@
 package io.basc.framework.aop.support;
 
+import java.util.Collections;
+import java.util.Map;
+
 import io.basc.framework.aop.MethodInterceptor;
 import io.basc.framework.aop.Proxy;
 import io.basc.framework.aop.ProxyFactory;
@@ -10,7 +13,7 @@ public final class FieldSetterListenUtils {
 
 	private static final Class<?>[] FIELD_SETTER_LISTEN_INTERFACES = new Class<?>[] { FieldSetterListen.class };
 
-	public static Proxy getFieldSetterListenProxy(ProxyFactory proxyFactory, Class<?> clazz) {
+	public static Proxy getProxy(ProxyFactory proxyFactory, Class<?> clazz) {
 		MethodInterceptor methodInterceptor = new FieldSetterListenMethodInterceptor();
 		return proxyFactory.getProxy(clazz, FIELD_SETTER_LISTEN_INTERFACES, methodInterceptor);
 	}
@@ -19,5 +22,26 @@ public final class FieldSetterListenUtils {
 		if (instance instanceof FieldSetterListen) {
 			((FieldSetterListen) instance)._clearFieldSetterMap();
 		}
+	}
+
+	public static Proxy getProxy(Class<?> type) {
+		return getProxy(ProxyUtils.getFactory(), type);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T newInstance(Class<T> type) {
+		return (T) getProxy(type).create();
+	}
+
+	/**
+	 * @param instance
+	 * @return 返回空说明这不是一个可以监听的实例
+	 */
+	public static Map<String, Object> getChangeMap(Object instance) {
+		if (instance instanceof FieldSetterListen) {
+			Map<String, Object> map = ((FieldSetterListen) instance)._getFieldSetterMap();
+			return map == null ? Collections.emptyMap() : Collections.unmodifiableMap(map);
+		}
+		return null;
 	}
 }
