ea01b52d27533b211e655393caa118f4aede3e87
舒超文 <shuchaowen@zx62580.com>
2022-01-10 09:54:49 +0800

    cleanup

---
+++ core/src/test/java/io/basc/framework/test/ReflectionApiTest.java
@@ -0,0 +1,47 @@
+package io.basc.framework.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.basc.framework.core.reflect.ReflectionApi;
+import io.basc.framework.core.reflect.ReflectionUtils;
+
+public class ReflectionApiTest {
+	@Test
+	public void test() throws Exception {
+		A a = ReflectionApi.allocateInstance(A.class);
+		assertTrue(a.a == 0);
+		A a1 = ReflectionApi.getConstructorForSerialization(A.class).newInstance();
+		assertTrue(a1.a == 0);
+		A a2 = ReflectionUtils.getConstructor(A.class).newInstance();
+		assertTrue(a2.a == 1);
+
+		B b = ReflectionApi.getConstructor(B.class).newInstance();
+		assertTrue(b.b == 0);
+
+		B b1 = ReflectionApi.allocateInstance(B.class);
+		assertTrue(b1.b == 0);
+
+		A aa = ReflectionApi.newInstance(A.class);
+		assertTrue(aa.a == 1);
+		B bb = ReflectionApi.newInstance(B.class);
+		assertTrue(bb.b == 0);
+	}
+
+	public static class A {
+		public int a;
+
+		public A() {
+			a = 1;
+		}
+	}
+
+	public static class B {
+		public int b;
+
+		public B(int b) {
+			this.b = b;
+		}
+	}
+}
