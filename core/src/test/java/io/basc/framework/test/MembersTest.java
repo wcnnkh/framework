f5b3551ff9ccd53026a52e697256607ed2bea4e2
舒超文 <shuchaowen@zx62580.com>
2022-01-10 09:11:40 +0800

    cleanup

--- core/src/test/java/io/basc/framework/test/MembersTest.java
+++ core/src/test/java/io/basc/framework/test/MembersTest.java
@@ -1,22 +1,26 @@
 package io.basc.framework.test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import io.basc.framework.core.reflect.ReflectionUtils;
 
 public class MembersTest {
-	
+
 	@Test
 	public void test() {
-		ReflectionUtils.getDeclaredFields(B.class).withAll().filter((e) -> e.getName().contentEquals("a")).streamAll().forEach((e) -> System.out.println(e));
+		assertTrue(ReflectionUtils.getDeclaredFields(B.class).withAll().filter((e) -> e.getName().equals("a"))
+				.streamAll().count() == 2);
 	}
-	
+
 	public static class A {
 		protected String a;
 		protected int b;
 	}
-	
-	public static class B extends A{
+
+	public static class B extends A {
+		protected int[] a;
 		public Object c;
 		public boolean d;
 	}
