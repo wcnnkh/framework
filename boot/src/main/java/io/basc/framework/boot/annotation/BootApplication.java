4da3a53e7ee1d3b0b420d40ec95c9d16c58fb2a5
舒超文 <shuchaowen@zx62580.com>
2022-01-26 15:50:49 +0800

    cleanup

---
+++ boot/src/main/java/io/basc/framework/boot/annotation/BootApplication.java
@@ -0,0 +1,19 @@
+package io.basc.framework.boot.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 启动类
+ * 
+ * @author wcnnkh
+ *
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BootApplication {
+}
