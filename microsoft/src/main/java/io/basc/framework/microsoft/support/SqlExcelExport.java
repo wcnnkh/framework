eca61e71f0f6483fc74ba88403444e52c8eec1ed
wcnnkh <416078075@qq.com>
2022-03-10 22:51:27 +0800

    cleanup

--- microsoft/src/main/java/io/basc/framework/microsoft/support/SqlExcelExport.java
+++ microsoft/src/main/java/io/basc/framework/microsoft/support/SqlExcelExport.java
@@ -2,14 +2,22 @@ package io.basc.framework.microsoft.support;
 
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import io.basc.framework.convert.Converter;
+import io.basc.framework.lang.Nullable;
 import io.basc.framework.microsoft.ExcelException;
 import io.basc.framework.microsoft.ExcelExport;
+import io.basc.framework.microsoft.ExcelVersion;
+import io.basc.framework.microsoft.MicrosoftUtils;
 import io.basc.framework.sql.Sql;
 import io.basc.framework.sql.SqlOperations;
+import io.basc.framework.sql.SqlUtils;
+import io.basc.framework.util.Assert;
 import io.basc.framework.util.stream.Processor;
 
 public class SqlExcelExport implements ExcelExport {
@@ -69,5 +77,36 @@ public class SqlExcelExport implements ExcelExport {
 
 	public static SqlExcelExport create(ExcelExport excelExport) {
 		return new SqlExcelExport(excelExport);
+	}
+
+	public static void export(SqlOperations sqlOperations, Sql sql, Object target, @Nullable ExcelVersion version,
+			Converter<Object, String> toString) throws IOException {
+		Assert.requiredArgument(sqlOperations != null, "sqlOperations");
+		Assert.requiredArgument(sql != null, "sql");
+		Assert.requiredArgument(target != null, "target");
+		Assert.requiredArgument(toString != null, "toString");
+		AtomicBoolean first = new AtomicBoolean(false);
+		ExcelExport excelExport = MicrosoftUtils.getExcelOperations().createExcelExport(target, version);
+		try {
+			sqlOperations.query(sql, (rs) -> {
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int count = rsmd.getColumnCount();
+				if (count == 0) {
+					return null;
+				}
+
+				if (first.compareAndSet(false, true)) {
+					excelExport.append(SqlUtils.getColumnNames(rsmd, count));
+				}
+
+				Object[] values = SqlUtils.getRowValues(rs, count);
+				String[] columns = toString.convert(values);
+				excelExport.append(columns);
+				return columns;
+			});
+		} finally {
+			excelExport.close();
+		}
+
 	}
 }
