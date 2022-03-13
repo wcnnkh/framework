1ff674a2d18b32073a0a5d2630bf7269485d6d75
wcnnkh <416078075@qq.com>
2022-03-13 12:24:38 +0800

    cleanup

--- core/src/main/java/io/basc/framework/io/FileRecords.java
+++ core/src/main/java/io/basc/framework/io/FileRecords.java
@@ -1,38 +1,26 @@
 package io.basc.framework.io;
 
-import java.io.EOFException;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Stream;
 
 import io.basc.framework.codec.Codec;
 import io.basc.framework.codec.support.RecordCodec;
-import io.basc.framework.logger.Logger;
-import io.basc.framework.logger.LoggerFactory;
 import io.basc.framework.util.Assert;
 import io.basc.framework.util.XUtils;
 import io.basc.framework.util.stream.CallableProcessor;
 import io.basc.framework.util.stream.ConsumerProcessor;
-import io.basc.framework.util.stream.ResponsiveIterator;
+import io.basc.framework.util.stream.Cursor;
 import io.basc.framework.util.stream.StreamProcessorSupport;
 
+/**
+ * 线程不安全的
+ * 
+ * @author wcnnkh
+ *
+ * @param <T>
+ */
 public final class FileRecords<T> {
-	private static final ExecutorService READ_EXECUTOR = Executors.newCachedThreadPool();
-
-	static {
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				READ_EXECUTOR.shutdown();
-			}
-		});
-	}
-
-	private static Logger logger = LoggerFactory.getLogger(FileRecords.class);
 	private final CallableProcessor<File, IOException> lazyCreator;
 	private volatile File file;
 	private final RecordCodec<T> codec;
@@ -84,29 +72,16 @@ public final class FileRecords<T> {
 		return false;
 	}
 
-	public Stream<T> stream() {
+	public Cursor<T> stream() {
 		if (file != null) {
 			synchronized (this) {
 				if (file != null) {
-					ResponsiveIterator<T> iterator = new ResponsiveIterator<T>();
-					READ_EXECUTOR.execute(() -> {
-						try {
-							consume((e) -> iterator.put(e));
-						} catch (Throwable e) {
-							logger.error(e, "read error");
-						} finally {
-							try {
-								iterator.close();
-							} catch (InterruptedException e) {
-								logger.error(e, "read thread error");
-							}
-						}
-					});
-					return XUtils.stream(iterator);
+					RecordIterator<T> iterator = new RecordIterator<T>(file, codec);
+					return StreamProcessorSupport.cursor(iterator).onClose(() -> iterator.close());
 				}
 			}
 		}
-		return StreamProcessorSupport.emptyStream();
+		return StreamProcessorSupport.emptyCursor();
 	}
 
 	/**
@@ -115,23 +90,18 @@ public final class FileRecords<T> {
 	 * @param consumer
 	 * @throws E
 	 */
-	public <E extends Throwable> void consume(ConsumerProcessor<T, E> consumer) throws E, IOException {
+	public <E extends Throwable> void consume(ConsumerProcessor<T, E> consumer) throws E {
 		Assert.requiredArgument(consumer != null, "consumer");
 		if (file != null) {
 			synchronized (this) {
 				if (file != null) {
-					FileInputStream fis = new FileInputStream(file);
+					RecordIterator<T> iterator = new RecordIterator<T>(file, codec);
 					try {
-						while (true) {
-							try {
-								T v = codec.decode(fis);
-								consumer.process(v);
-							} catch (EOFException e) {
-								break;
-							}
+						while (iterator.hasNext()) {
+							consumer.process(iterator.next());
 						}
 					} finally {
-						fis.close();
+						iterator.close();
 					}
 				}
 			}
--- core/src/main/java/io/basc/framework/io/RecordIterator.java
+++ core/src/main/java/io/basc/framework/io/RecordIterator.java
@@ -1,6 +1,8 @@
 package io.basc.framework.io;
 
 import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
@@ -11,6 +13,7 @@ import io.basc.framework.codec.DecodeException;
 import io.basc.framework.codec.support.RecordCodec;
 import io.basc.framework.util.Assert;
 import io.basc.framework.util.StaticSupplier;
+import io.basc.framework.util.stream.CallableProcessor;
 
 /**
  * 线程不安全
@@ -19,26 +22,62 @@ import io.basc.framework.util.StaticSupplier;
  *
  * @param <E>
  */
-public class RecordIterator<E> implements Iterator<E> {
-	private final InputStream source;
+public final class RecordIterator<E> implements Iterator<E>, AutoCloseable {
+	private final CallableProcessor<InputStream, IOException> sourceSupplier;
 	private final RecordCodec<E> codec;
+	private volatile InputStream source;
 	private volatile Supplier<E> supplier;
 
-	public RecordIterator(InputStream source, RecordCodec<E> codec) {
-		Assert.requiredArgument(source != null, "source");
+	public RecordIterator(File file, RecordCodec<E> codec) {
+		this(() -> new FileInputStream(file), codec);
+	}
+
+	public RecordIterator(CallableProcessor<InputStream, IOException> sourceSupplier, RecordCodec<E> codec) {
+		Assert.requiredArgument(sourceSupplier != null, "sourceSupplier");
 		Assert.requiredArgument(codec != null, "codec");
-		this.source = source;
+		this.sourceSupplier = sourceSupplier;
 		this.codec = codec;
+	}
+
+	private InputStream getSource() throws IOException {
+		if (source == null) {
+			synchronized (this) {
+				if (source == null) {
+					source = sourceSupplier.process();
+				}
+			}
+		}
+		return source;
+	}
+
+	@Override
+	public void close() {
+		if (source != null) {
+			synchronized (this) {
+				if (source != null) {
+					try {
+						IOUtils.closeQuietly(source);
+					} finally {
+						source = null;
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public boolean hasNext() {
 		if (supplier == null) {
 			try {
-				supplier = new StaticSupplier<E>(codec.decode(source));
+				supplier = new StaticSupplier<E>(codec.decode(getSource()));
 			} catch (EOFException e) {
+				close();
 				return false;
-			} catch (IOException e) {
+			} catch (RuntimeException e) {
+				close();
+				throw e;
+			} catch (Throwable e) {
+				close();
 				throw new DecodeException(e);
 			}
 		}
