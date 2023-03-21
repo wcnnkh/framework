cbb97c0fc5da889cc3b25ba8f4140ea281d0cb81
wcnnkh <247044007@qq.com>
2023-03-21 23:44:52 +0800

    cleanup

--- core/src/main/java/io/basc/framework/logger/JdkLoggerFactory.java
+++ core/src/main/java/io/basc/framework/logger/JdkLoggerFactory.java
@@ -4,22 +4,13 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
 public class JdkLoggerFactory implements ILoggerFactory {
-	private static final Logger ROOT_LOGGER = Logger.getLogger(JdkLoggerFactory.class.getName());
-
-	static {
-		ROOT_LOGGER.addHandler(new ConsoleHandler());
-		ROOT_LOGGER.setUseParentHandlers(false);
-	}
-
-	public static Logger getRootLogger() {
-		return ROOT_LOGGER;
-	}
 
 	@Override
 	public io.basc.framework.logger.Logger getLogger(String name) {
 		Logger jdkLogger = Logger.getLogger(name);
-		if (jdkLogger != ROOT_LOGGER) {
-			jdkLogger.setParent(ROOT_LOGGER);
+		if (jdkLogger.getHandlers().length == 0) {
+			jdkLogger.addHandler(new ConsoleHandler());
+			jdkLogger.setUseParentHandlers(false);
 		}
 		return new JdkLogger(jdkLogger);
 	}
