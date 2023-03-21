a24d7a47de55d76635ef1d67e44aa6ec859481d9
wcnnkh <247044007@qq.com>
2023-03-21 11:07:43 +0800

    cleanup

--- core/src/main/java/io/basc/framework/util/CollectionUtils.java
+++ core/src/main/java/io/basc/framework/util/CollectionUtils.java
@@ -439,31 +439,6 @@ public abstract class CollectionUtils {
 		return result;
 	}
 
-	/**
-	 * Determine whether the given Collection only contains a single unique object.
-	 * 
-	 * @param collection the Collection to check
-	 * @return {@code true} if the collection contains a single reference or
-	 *         multiple references to the same instance, {@code false} else
-	 */
-	@SuppressWarnings("rawtypes")
-	public static boolean hasUniqueObject(Collection collection) {
-		if (isEmpty(collection)) {
-			return false;
-		}
-		boolean hasCandidate = false;
-		Object candidate = null;
-		for (Object elem : collection) {
-			if (!hasCandidate) {
-				hasCandidate = true;
-				candidate = elem;
-			} else if (candidate != elem) {
-				return false;
-			}
-		}
-		return true;
-	}
-
 	public static <E> Collection<E> intersection(Iterable<? extends E> leftIterable,
 			Iterable<? extends E> rightIterable) {
 		if (isEmpty(leftIterable) || isEmpty(rightIterable)) {
