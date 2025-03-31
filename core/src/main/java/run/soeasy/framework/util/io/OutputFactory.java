f1cf80c037f732d82b80088b80472e15e59d3191
wcnnkh <416078075@qq.com>
2025-03-31 12:54:39 +0800

    cleanup

--- core/src/main/java/run/soeasy/framework/util/io/OutputFactory.java
+++ core/src/main/java/run/soeasy/framework/util/io/OutputFactory.java
@@ -1,10 +1,55 @@
 package run.soeasy.framework.util.io;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import run.soeasy.framework.util.function.Function;
+import run.soeasy.framework.util.function.Pipeline;
 
 public interface OutputFactory<O extends OutputStream, W extends Writer>
 		extends OutputStreamFactory<O>, WriterFactory<W> {
 	@Override
 	boolean isEncoded();
+
+	@RequiredArgsConstructor
+	@Getter
+	public static class DefaultOutputFactory<O extends OutputStream, S extends OutputStreamFactory<? extends O>, W extends Writer, T extends WriterFactory<? extends W>>
+			implements OutputFactory<O, W> {
+		protected final S outputStreamFactory;
+		protected final T writerFactory;
+
+		@Override
+		public @NonNull Pipeline<O, IOException> getOutputStreamPipeline() {
+			return outputStreamFactory == null ? Pipeline.empty()
+					: outputStreamFactory.getOutputStreamPipeline().map(Function.identity());
+		}
+
+		@Override
+		public @NonNull Pipeline<W, IOException> getWriterPipeline() {
+			return writerFactory == null ? Pipeline.empty()
+					: writerFactory.getWriterPipeline().map(Function.identity());
+		}
+
+		@Override
+		public boolean isEncoded() {
+			return writerFactory != null;
+		}
+
+		@Override
+		public OutputFactory<O, Writer> encode() {
+			if (writerFactory != null) {
+				return new DefaultOutputFactory<>(outputStreamFactory, writerFactory);
+			}
+			return OutputFactory.super.encode();
+		}
+	}
+
+	public static <O extends OutputStream, W extends Writer> OutputFactory<O, W> forFactory(
+			OutputStreamFactory<? extends O> outputStreamFactory, WriterFactory<? extends W> writerFactory) {
+		return new DefaultOutputFactory<>(outputStreamFactory, writerFactory);
+	}
 }
--- core/src/main/java/run/soeasy/framework/util/io/OutputSource.java
+++ core/src/main/java/run/soeasy/framework/util/io/OutputSource.java
@@ -1,9 +1,32 @@
 package run.soeasy.framework.util.io;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
 
 public interface OutputSource<O extends OutputStream, W extends Writer>
 		extends OutputFactory<O, W>, OutputStreamSource<O>, WriterSource<W> {
-	
+	public static class DefaultOutputSource<O extends OutputStream, S extends OutputStreamSource<? extends O>, W extends Writer, T extends WriterSource<? extends W>>
+			extends DefaultOutputFactory<O, S, W, T> implements OutputSource<O, W> {
+
+		public DefaultOutputSource(S outputStreamFactory, T writerFactory) {
+			super(outputStreamFactory, writerFactory);
+		}
+
+		@Override
+		public O getOutputStream() throws IOException {
+			return outputStreamFactory == null ? null : outputStreamFactory.getOutputStream();
+		}
+
+		@Override
+		public W getWriter() throws IOException {
+			return writerFactory == null ? null : writerFactory.getWriter();
+		}
+
+	}
+
+	public static <O extends OutputStream, W extends Writer> OutputSource<O, W> forSource(
+			OutputStreamSource<? extends O> outputStreamSource, WriterSource<? extends W> writerSource) {
+		return new DefaultOutputSource<>(outputStreamSource, writerSource);
+	}
 }
